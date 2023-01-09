336a3ee8636f9d71ed84d7d4057fe6f23269020c
shawon chowdhury <shawon.c@duke.edu>
2023-01-09 13:20:30 -0500

    terms updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -133,7 +133,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2022 Kahana Group Inc. All rights reserved.
+            &copy; 2023 Kahana Group Inc. All rights reserved.
           </p>
         </div>
       </div>
