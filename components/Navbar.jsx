b98b8cb80fba4297a4e1a7c0140afabb5241300f
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-03-08 20:08:01 -0600

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -56,7 +56,7 @@ function Navbar() {
           </div>
           {/* mobile menu */}
 
-          <div className="flex flex-wrap justify-center space-x-6 py-4 lg:hidden">
+          <div className="flex flex-wrap justify-start space-x-5 py-4 lg:hidden">
             {navigation.map((link) => (
               <Link
                 href={link.href}
