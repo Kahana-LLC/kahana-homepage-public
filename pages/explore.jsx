e5fd22de4822cd0d43b0dab966e573180ca0af1c
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-04-11 19:29:01 -0500

    Update explore.jsx

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -192,16 +192,6 @@ export default function Explore() {
   return (
     <>
       <Head>
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2868036,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
         <title>Kahana - Helping Creators Monetize</title>
         <meta
           name="Kahana"
