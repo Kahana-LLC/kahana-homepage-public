4e7839dafb1fe859496d5f3efa60b25935755feb
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-04-20 15:43:36 -0500

    Update SimpleHeader.jsx

--- components/SimpleHeader.jsx
+++ components/SimpleHeader.jsx
@@ -4,11 +4,11 @@ export default function SimpleHeader() {
       <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="text-center">
           <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
-            We now live in a creator economy
+            We now live in a knowledge economy
           </p>
           <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
-            YouTube coined the term &quot;creators&quot; in 2011. Today Linktree
-            reports over 200,000,000 creators counted globally.
+            Over 200M people worldwide are trying to make a living from sharing 
+            their passions and expertise, but...
           </p>
         </div>
       </div>
