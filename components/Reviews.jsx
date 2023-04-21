baa5c91325f63eac7630271774b2444914a2618d
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-04-21 15:16:41 -0500

    Update Reviews.jsx

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -17,10 +17,10 @@ const testimonials = [
     },
     {
       content:
-        "I can create hubs for clients and for my own newsletters, blog posts, and scripts for webinars. There's so much information overload in today's day and age that leads to disorganization and chaos. Kahana wrangles that in.",
+        "I can honestly say that I've never worked with a team that was as helpful and dedicated as the team at Kahana. If you're a digital creator looking for a platform that not only supports your content creation but also helps you achieve your goals, then Kahana is the answer. Jonathan and Adam are truly amazing, and I can't thank them enough for all that they've done for me.",
       author: {
-        name: 'Content Creator',
-        role: 'Soul Story Creative',
+        name: 'Olivia Mancuso',
+        {/* role: 'Soul Story Creative', */}
         image: avatarImage2,
       },
     },
