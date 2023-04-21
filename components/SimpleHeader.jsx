477f6228510e8b3fe0b3f14e1c82e5e14ba099e5
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-04-21 13:41:32 -0500

    Update SimpleHeader.jsx

--- components/SimpleHeader.jsx
+++ components/SimpleHeader.jsx
@@ -4,7 +4,7 @@ export default function SimpleHeader() {
       <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="text-center">
           <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
-            We now live in a knowledge economy
+            Where the Creator Economy meets the Knowledge Economy
           </p>
           <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
             Over 200M people worldwide are trying to make a living from sharing 
