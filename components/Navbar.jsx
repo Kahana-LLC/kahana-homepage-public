44b22183f515db75e7da35590efef44e3a4f940c
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-04 15:02:47 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,14 +45,14 @@ function Navbar() {
                 ))}
               </div>
             </div>
-            {/* <div className="ml-10 space-x-4 ">
+            <div className="ml-10 space-x-4 ">
             <a
               href="/login"
               className="inline-block justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] drop-shadow-2xl"
             >
               Log in
             </a>
-          </div> */}
+          </div>
           </div>
           {/* mobile menu */}
 
