b37838a3a351853aaa5b37234854420cf9219257
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-08 16:51:06 -0500

    Update solutions.jsx

--- pages/solutions.jsx
+++ pages/solutions.jsx
@@ -62,11 +62,11 @@ const posts = [
   },
 ];
 //
-export default function Explore() {
+export default function Solutions() {
   return (
     <>
       <Head>
-        <title>Kahana - Helping Creators Monetize</title>
+        <title>Kahana - Get paid for your best stuff</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
