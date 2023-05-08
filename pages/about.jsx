1e5ce924d825e91742d2b4e2164bb61dd6fe6339
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-08 17:21:23 -0500

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -12,7 +12,7 @@ export default function About() {
   return (
     <>
       <Head>
-        <title>Kahana - Helping Creators Monetize</title>
+        <title>Why we built Kahana - About</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
