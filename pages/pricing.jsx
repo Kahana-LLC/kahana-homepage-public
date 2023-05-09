ffbcbe91dc87cc2178831c2f4358f297f5b9b8cf
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-08 21:58:19 -0500

    Update pricing.jsx

--- pages/pricing.jsx
+++ pages/pricing.jsx
@@ -11,7 +11,7 @@ export default function PricingPage() {
   return (
     <>
       <Head>
-        <title>Kahana - Helping Creators Monetize</title>
+        <title>Kahana - Get paid for your best stuff</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
