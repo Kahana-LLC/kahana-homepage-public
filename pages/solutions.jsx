ad2a1291666c0164cd318adfd1a68ef048b5516a
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-10 20:22:42 -0500

    Update solutions.jsx

--- pages/solutions.jsx
+++ pages/solutions.jsx
@@ -165,7 +165,7 @@ export default function Solutions() {
                     <div class="flex justify-center">
                       <a
                         href={post.buttonLink}
-                        className="flex w-1/2 justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324]"
+                        className="flex w-1/2 justify-center rounded-md border border-transparent bg-[#038270] py-6 px-6 text-sm font-medium text-white shadow-sm hover:bg-[#024324]"
                       >
                           {post.buttonQuote}
                       </a>
