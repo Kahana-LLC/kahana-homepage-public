8943fee725022a4ccc61c7f205fefdf3cea93bc2
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-10 20:10:18 -0500

    Update solutions.jsx

--- pages/solutions.jsx
+++ pages/solutions.jsx
@@ -165,7 +165,7 @@ export default function Solutions() {
                     <div>
                       <a
                         href={post.buttonLink}
-                        className="flex w-1/2 justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 mb-3"
+                        className="flex w-1/2 justify-center rounded-md border border-transparent bg-[#038270] m-2 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 mb-3"
                       >
                           {post.buttonQuote}
                       </a>
