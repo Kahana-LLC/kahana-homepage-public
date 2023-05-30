c70e39b2649f8a7c1c8efd209751c4b607280362
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-30 17:56:52 -0500

    Update sitemap.jsx

--- pages/sitemap.jsx
+++ pages/sitemap.jsx
@@ -5,17 +5,8 @@
 
       import React from 'react';
 
-    export default function MySitemap() {
-     return (
-      <>
-       <Head>
-        <title>Kahana - Get paid for your best stuff</title>
-        <meta
-          name="Kahana"
-          content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
-        />
-       </Head>
-       <NavbarDup />
+      const Sitemap = () => {
+       return (
         <div>
           <style>{`
             h1 {
@@ -160,6 +151,21 @@
 <li><a href="https://blog.kahana.co/thoughts-on-the-state-of-the-consulting-industry/">Thoughts On The State Of The Consulting Industry</a></li>
           </ul>
         </div>
+      );
+     };
+
+    export default function MySitemap() {
+     return (
+      <>
+       <Head>
+        <title>Kahana - Get paid for your best stuff</title>
+        <meta
+          name="Kahana"
+          content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
+        />
+       </Head>
+       <NavbarDup />
+       <Sitemap />
         <Footer />
        </>
       );
