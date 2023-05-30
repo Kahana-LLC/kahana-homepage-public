abcc5f549f99fa50b5a62cc9046c0742f9a7501e
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-30 18:42:35 -0500

    Update sitemap.jsx

--- pages/sitemap.jsx
+++ pages/sitemap.jsx
@@ -1,9 +1,284 @@
-      import Head from 'next/head';
+import Head from 'next/head';
+import React from 'react';
 
-      import Footer from '../components/Footer';
-      import NavbarDup from '../components/NavbarDup';
+import Image from 'next/image';
+import Link from 'next/link';
 
-      import React from 'react';
+//images
+import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
+import grayKahanaLogo from '../assets/kahana_logo_wide_gray.svg';
+import HeaderBanner from './HeaderBanner';
+import { useState } from "react"; // import state
+
+//navigation items
+const navigation = [
+  { name: 'Featured Hubs', href: 'explore' },
+  { name: 'About', href: 'about' },
+  { name: 'Pricing', href: 'pricing' },
+  { name: 'Blog', href: ' https://blog.kahana.co' },
+  { name: 'Solutions', href: 'solutions' },
+];
+
+const navigation1 = [
+  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
+  { name: 'Log in', href: 'https://app.kahana.co/login' },
+];
+
+const navigationAll = [
+  { name: 'Featured Hubs', href: 'explore' },
+  { name: 'About', href: 'about' },
+  { name: 'Pricing', href: 'pricing' },
+  { name: 'Blog', href: ' https://blog.kahana.co' },
+  { name: 'Solutions', href: 'solutions' },
+  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
+  { name: 'Log in', href: 'https://app.kahana.co/login' },
+];
+
+const navigationFooter = {
+  important: [
+    { name: 'Explore', href: '/explore' },
+
+    { name: 'Blog', href: 'https://blog.kahana.co' },
+    { name: 'About', href: '/about' },
+    { name: 'Home', href: '/' },
+    { name: 'Careers', href: 'https://7hkdcfzbmr0.typeform.com/to/RQ99b3Bp' },
+    { name: 'Invest in seed round', href: 'https://7hkdcfzbmr0.typeform.com/to/wYCUMm54' },
+    // { name: 'Log in', href: '#' },
+    // { name: 'Sign up', href: '#' },
+  ],
+  support: [
+    { name: 'Help center', href: 'https://kahana.tawk.help/' },
+    { name: 'Vote for features', href: 'https://productific.com/@Kahana' },
+    { name: 'Live chat', href: 'https://kahana.tawk.help/' },
+    { name: 'Join the community', href: 'https://nas.io/creators-and-experts' },
+  ],
+  social: [
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/company/kahana-co/' },
+    {
+      name: 'YouTube',
+      href: 'https://www.youtube.com/channel/UCwsf3DOnt3uQdrqf-NRZ2_w?sub_confirmation=1',
+    },
+    {
+      name: 'Instagram',
+      href: 'https://instagram.com/kahanahq',
+    },
+    {
+      name: 'TikTok',
+      href: 'https://www.tiktok.com/@kahanahub',
+    },
+    { name: 'Twitter', href: 'https://twitter.com/KahanaHQ' },
+  ],
+  legal: [
+    { name: 'Privacy Policy', href: '/privacy-policy' },
+    { name: 'Terms & Conditions', href: '/terms-and-conditions' },
+  ],
+};
+
+cost Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+  return (
+    <>
+      <HeaderBanner />
+      <header className="backdrop-blur-sm bg-white/90 bg-white  mx-auto px-2">
+      <nav>
+      <div className="flex items-center justify-between border-b py-2 px-10">
+      {/* <a href="/">
+        <img src="https://designbygio.it/images/logo.png" alt="logo" />
+      </a> */}
+      <Link href="/" aria-label="Home">
+         <span className="sr-only"> Company</span>
+         <Image
+            className="h-10 "
+            src={whiteKahanaLogo}
+            w
+            // width={10}
+            // height={20} 
+            alt="navbar-logo"
+         />
+      </Link>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> {/* toggle class based on isNavOpen state */}
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              {navigationAll.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.name}
+                  className="text-base font-small text-gray-600  hover:text-gray-800"
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+          {navigationAll.map((link) => (
+             <Link
+                href={link.href}
+                key={link.name}
+                className="text-base font-small text-gray-600  hover:text-gray-800"
+              >
+                {link.name}
+              </Link>
+           ))}
+        </ul>
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+    </div>
+    </nav>
+    </header>
+    </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="bg-white" aria-labelledby="footer-heading">
+      <hr className="my-8 h-px bg-slate-300 border-0 mx-8" />
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="space-y-8 xl:col-span-1">
+            <Image
+              className="h-10 "
+              src={grayKahanaLogo}
+              w
+              // width={10}
+              // height={20}
+              alt="navbar-logo"
+            />
+            <p className="text-base text-gray-500">
+              Reserve your account and join 2000+ others learning to monetize
+              their expertise. Reservers also get early-bird access to tips,
+              tricks, templates, plus more.
+            </p>
+          </div>
+          <div className="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-base font-medium text-gray-900">
+                  Important links
+                </h3>
+                <ul role="list" className="mt-4 space-y-4">
+                  {navigationFooter.important.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-base font-medium text-gray-900">Support</h3>
+                <ul role="list" className="mt-4 space-y-4">
+                  {navigationFooter.support.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-base font-medium text-gray-900">Social</h3>
+                <ul role="list" className="mt-4 space-y-4">
+                  {navigationFooter.social.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-base font-medium text-gray-900">Legal</h3>
+                <ul role="list" className="mt-4 space-y-4">
+                  {navigationFooter.legal.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-12 border-t border-gray-200 pt-8">
+          <p className="text-base text-gray-400 xl:text-center">
+            &copy; 2023 Kahana Group Inc. All rights reserved. <Link href="/sitemap">Sitemap</Link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
       const Sitemap = () => {
        return (
@@ -156,14 +431,14 @@
      return (
       <>
        <Head>
-        <title>Kahana - Get paid for your best stuff</title>
+        <title>Kahana Sitemap</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
         />
        </Head>
        <div>
-         <NavbarDup />
+         <Header />
        </div>
        <div>
          <Sitemap />
