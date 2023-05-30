f18c62ac35288b52927fd508e7290606963e34be
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-30 01:00:08 -0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image';
 import grayKahanaLogo from '../assets/kahana_logo_wide_gray.svg';
 
@@ -136,7 +137,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2023 Kahana Group Inc. All rights reserved. <a href="/sitemap">Sitemap</a>
+            &copy; 2023 Kahana Group Inc. All rights reserved. <Link href="/sitemap">Sitemap</Link>
           </p>
         </div>
       </div>
