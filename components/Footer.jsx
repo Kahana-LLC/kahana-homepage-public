15d9511caee120dec72fcfe46ef1d17c417a8b3c
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-05-30 00:55:17 -0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -136,7 +136,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2023 Kahana Group Inc. All rights reserved.   
+            &copy; 2023 Kahana Group Inc. All rights reserved. <a href="/sitemap">Sitemap</a>
           </p>
         </div>
       </div>
