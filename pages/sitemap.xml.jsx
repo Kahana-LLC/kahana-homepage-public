9f04755ed87f9264ba310aaa28956802891714fa
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-01 14:52:05 -0500

    Update sitemap.xml.jsx

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Sitemap = () => {
+  const [xmlContent, setXmlContent] = useState('');
+
+  useEffect(() => {
+    fetch('https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml')
+      .then(response => response.text())
+      .then(data => setXmlContent(data));
+  }, []);
+
   return (
-    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
-      <iframe
-        src="https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml"
-        title="Sitemap"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          border: 'none'
-        }}
-      ></iframe>
+    <div>
+      <pre>{xmlContent}</pre>
     </div>
   );
 };
 
 export default Sitemap;
+
