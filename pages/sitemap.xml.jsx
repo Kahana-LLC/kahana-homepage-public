019cf4b2de2d6abbbac9085b9ed13f4c873d3659
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-01 14:48:41 -0500

    Update sitemap.xml.jsx

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Sitemap = () => {
   return (
-    <div>
-      <iframe src="https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml" style={{ width: '100%', height: '100vh', border: 'none' }}></iframe>
+    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+      <iframe
+        src="https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml"
+        title="Sitemap"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 'none'
+        }}
+      ></iframe>
     </div>
   );
 };
