d7cab2d2c4164d34c49599d4c58983e44c4a86c7
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-01 18:30:27 -0500

    Update sitemap.xml.jsx

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -1,11 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SitemapRedirect = () => {
+const SitemapXml = () => {
+  const [xmlContent, setXmlContent] = useState('');
+
   useEffect(() => {
-    window.location.href = 'https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml';
+    const fetchXmlContent = async () => {
+      try {
+        const response = await fetch('https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml');
+        const xml = await response.text();
+        setXmlContent(xml);
+      } catch (error) {
+        console.error('Error fetching XML content:', error);
+      }
+    };
+
+    fetchXmlContent();
   }, []);
 
-  return null;
+  return (
+    <div>
+      <pre className="xml-viewer-style">
+        <code className="webkit-xml-viewer-source-xml">
+          {xmlContent}
+        </code>
+      </pre>
+    </div>
+  );
 };
 
-export default SitemapRedirect;
+export default SitemapXml;
+
