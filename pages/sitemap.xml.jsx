6b2652d6a6fdf7f1b512144c2d0b95e0c7f00009
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-02 09:08:40 -0500

    Create sitemap.xml.jsx

---
+++ pages/sitemap.xml.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+function SitemapViewer() {
+  const [xmlContent, setXmlContent] = useState('');
+
+  useEffect(() => {
+    const fetchXml = async () => {
+      try {
+        const response = await fetch('https://kahana-sitemap-xml.s3.us-east-2.amazonaws.com/sitemap.xml');
+        const xml = await response.text();
+        setXmlContent(xml);
+      } catch (error) {
+        console.log('Error fetching XML:', error);
+      }
+    };
+
+    fetchXml();
+  }, []);
+
+  return (
+    <div>
+      <pre>{xmlContent}</pre>
+    </div>
+  );
+}
+
+export default SitemapViewer;
