fde07e10fe2278702cd9ec36e1e8909af1bf1ce6
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-06 14:25:48 -0500

    Update privacy-policy.jsx

--- pages/privacy-policy.jsx
+++ pages/privacy-policy.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 /* eslint-disable react/no-unescaped-entities */
 // import Image from 'next/image';
 
@@ -7,6 +8,25 @@ import NavbarDup from '../components/NavbarDup';
 export default function PrivacyPolicy() {
   return (
     <>
+      <Head>
+        <title>Kahana Privacy Policy</title>
+        <meta
+          name="Kahana"
+          content="Kahana is the easiest way to turn your knowledge into subscription revenue. Sign up for free today!"
+        />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KQHFL9605P"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KQHFL9605P');
+            `,
+          }}
+        />
+      </Head>
       <div style={{ zIndex: '2' }} className="sticky top-0">
         <NavbarDup />
       </div>
