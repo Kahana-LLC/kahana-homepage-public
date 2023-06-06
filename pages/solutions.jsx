6e63c379b85285fa912597dbeb9c0c631c34ad1a
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-06 12:10:19 -0500

    Update solutions.jsx

--- pages/solutions.jsx
+++ pages/solutions.jsx
@@ -78,7 +78,7 @@ export default function Solutions() {
   return (
     <>
       <Head>
-        <title>Kahana - Get paid for your best stuff</title>
+        <title>Kahana Solutions</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
