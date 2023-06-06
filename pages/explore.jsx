dffa10a14e92ddcefbd524366635312b388e43ec
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-06 11:59:40 -0500

    Update explore.jsx

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -246,7 +246,7 @@ export default function Explore() {
   return (
     <>
       <Head>
-        <title>Kahana - Get paid for your best stuff</title>
+        <title>Explore Kahana Hubs</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today! "
