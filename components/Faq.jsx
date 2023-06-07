274970a412323b7c2eb91b327bf074206e91d019
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-07 00:21:42 -0500

    Update Faq.jsx

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -1,4 +1,58 @@
+import React from 'react';
+
 const faqs = [
+  {
+    question: 'What is Kahana?',
+    answer: (
+      <span>
+        You can think of Kahana as Patreon meets Google Drive - it's a collaborative platform that lets you create hubs of knowledge with other creators & experts and monetize <i>together</i>. You can upload your best knowledge (information you've learned, notes, methodologies, best practices, and templates you've curated and created, etc.) and charge for access to it without having to build something from scratch.
+      </span>
+    ),
+  },
+  {
+    question: 'Is Kahana free to use?',
+    answer: 'Yes! You can use Kahana for free, forever.',
+  },
+  {
+    question: 'What does a hub look like?',
+    answer: (
+      <span>
+        You can check out our{' '}
+        <a href="https://kahana.co/explore" className="text-[#038270] font-medium">
+          Featured Hubs
+        </a>{' '}
+        page for examples of what a hub looks like.
+      </span>
+    ),
+  },
+  {
+    question: 'How do I think about starting a hub?',
+    answer: (
+      <span>
+        The first step is to recognize that you have unique knowledge! Everyone has a topic, field, or skill that they could teach someone who was in their shoes two years ago, a year ago, or even six months ago. If you need help finding yours, we put together a{' '}
+        <a href="https://blog.kahana.co/how-to-create-your-first-digital-product/" className="text-[#038270] font-medium">
+          step-by-step guide
+        </a>{' '}
+        to show you how you can quickly identify something you know a lot about and are passionate about, and turn it into a hub.
+      </span>
+    ),
+  },
+  {
+    question: 'How do I get paid from Kahana?',
+    answer: (
+      <span>
+        In a nutshell, you earn money when people subscribe to your monetized hubs. In order to charge for access to your hubs, you can connect to Stripe, which you can do by following{' '}
+        <a href="https://blog.kahana.co/connect-stripe/" className="text-[#038270] font-medium">
+          these steps
+        </a>
+        .
+      </span>
+    ),
+  },
+  // More questions...
+];
+
+/* const faqs = [
   {
     question: 'What is Kahana?',
     answer:
@@ -26,7 +80,7 @@ const faqs = [
   },
 
   // More questions...
-];
+]; */
 
 export default function Faq() {
   return (
