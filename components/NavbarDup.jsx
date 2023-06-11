2000252c77f40f87e423de6e9ffcb2f7769f6027
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-11 18:29:02 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -1,30 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 //images
 import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 import HeaderBanner from './HeaderBanner';
-import { useState } from "react"; // import state
 
 //navigation items
-const navigation = [
-  { name: 'Featured Hubs', href: 'explore' },
-  { name: 'About', href: 'about' },
-  { name: 'Pricing', href: 'pricing' },
-  { name: 'Blog', href: ' https://blog.kahana.co' },
-  { name: 'Solutions', href: 'solutions' },
-];
-
-const navigation1 = [
-  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
-  { name: 'Log in', href: 'https://app.kahana.co/login' },
-];
-
 const navigationAll = [
   { name: 'Featured Hubs', href: 'explore' },
   { name: 'About', href: 'about' },
   { name: 'Pricing', href: 'pricing' },
-  { name: 'Blog', href: ' https://blog.kahana.co' },
+  { name: 'Blog', href: 'https://blog.kahana.co' },
   { name: 'Solutions', href: 'solutions' },
   { name: 'FAQ', href: 'faq' },
   { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
@@ -32,104 +19,109 @@ const navigationAll = [
 ];
 
 export default function NavbarDup() {
-  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <>
       <HeaderBanner />
-      <header className="backdrop-blur-sm bg-white/90 bg-white  mx-auto px-2">
-      <nav>
-      <div className="flex items-center justify-between border-b py-2 px-10">
-      {/* <a href="/">
-        <img src="https://designbygio.it/images/logo.png" alt="logo" />
-      </a> */}
-      <Link href="/" aria-label="Home">
-         <span className="sr-only"> Company</span>
-         <Image
-            className="h-10 "
-            src={whiteKahanaLogo}
-            w
-            // width={10}
-            // height={20} 
-            alt="navbar-logo"
-         />
-      </Link>
-      <nav>
-        <section className="MOBILE-MENU flex lg:hidden">
-          <div
-            className="HAMBURGER-ICON space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
-          >
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-          </div>
-
-          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> {/* toggle class based on isNavOpen state */}
-            <div
-              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
-            >
-              <svg
-                className="h-8 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-              {navigationAll.map((link) => (
-                <Link
-                  href={link.href}
-                  key={link.name}
-                  className="text-base font-small text-gray-600  hover:text-gray-800"
+      <header className="backdrop-blur-sm bg-white/90 bg-white mx-auto px-2">
+        <nav>
+          <div className="flex items-center justify-between border-b py-2 px-10">
+            <Link href="/" aria-label="Home">
+              <span className="sr-only">Company</span>
+              <Image
+                className="h-10"
+                src={whiteKahanaLogo}
+                w
+                alt="navbar-logo"
+              />
+            </Link>
+            <nav>
+              <section className="MOBILE-MENU flex lg:hidden">
+                <div
+                  className={`HAMBURGER-ICON ${isNavOpen ? 'open' : ''}`}
+                  onClick={() => setIsNavOpen((prev) => !prev)}
                 >
-                  {link.name}
-                </Link>
-              ))}
-            </ul>
-          </div>
-        </section>
+                  <span className="block h-0.5 w-5 bg-gray-600"></span>
+                  <span className="block h-0.5 w-5 bg-gray-600"></span>
+                  <span className="block h-0.5 w-5 bg-gray-600"></span>
+                </div>
 
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
-          {navigationAll.map((link) => (
-             <Link
-                href={link.href}
-                key={link.name}
-                className="text-base font-small text-gray-600  hover:text-gray-800"
-              >
-                {link.name}
-              </Link>
-           ))}
-        </ul>
-      </nav>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </div>
-    </nav>
-    </header>
+                <div className={`MENU-LINK-MOBILE ${isNavOpen ? 'open' : ''}`}>
+                  {navigationAll.map((link) => (
+                    <Link
+                      href={link.href}
+                      key={link.name}
+                      className="text-base font-small text-gray-600 hover:text-gray-800"
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                </div>
+              </section>
+
+              <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+                {navigationAll.map((link) => (
+                  <Link
+                    href={link.href}
+                    key={link.name}
+                    className="text-base font-small text-gray-600 hover:text-gray-800"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </nav>
+      </header>
+      <style jsx>{`
+        .HAMBURGER-ICON {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          cursor: pointer;
+          height: 18px;
+        }
+
+        .HAMBURGER-ICON.open span:first-child {
+          transform: rotate(45deg) translate(1px, 1px);
+        }
+
+        .HAMBURGER-ICON.open span:nth-child(2) {
+          opacity: 0;
+        }
+
+        .HAMBURGER-ICON.open span:last-child {
+          transform: rotate(-45deg) translate(0, -1px);
+        }
+
+        .MOBILE-MENU {
+          position: relative;
+        }
+
+        .MENU-LINK-MOBILE {
+          display: none;
+          position: absolute;
+          top: 100%;
+          left: 0;
+          right: 0;
+          padding: 1rem;
+          background-color: white;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+          border-radius: 4px;
+        }
+
+        .MENU-LINK-MOBILE.open {
+          display: block;
+        }
+
+        .DESKTOP-MENU {
+          flex-grow: 1;
+          justify-content: flex-end;
+        }
+      `}</style>
     </>
   );
 }
