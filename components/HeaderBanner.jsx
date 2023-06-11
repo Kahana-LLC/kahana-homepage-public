183be6a4e379219dc9c7c6a90e06e99b7721ddb7
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-11 17:20:50 -0500

    Update HeaderBanner.jsx

--- components/HeaderBanner.jsx
+++ components/HeaderBanner.jsx
@@ -1,6 +1,6 @@
 export default function HeaderBanner() {
   return (
-    <div className="relative">
+    <div className="relative bg-emerald-700">
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:px-16 sm:text-center">
           <p className="text-base text-white">
@@ -9,7 +9,7 @@ export default function HeaderBanner() {
             <span className="block sm:ml-2 sm:inline-block">
               <a
                 href="https://app.kahana.co/signup"
-                className="text-base text-white underline"
+                className=" text-base text-white underline"
               >
                 Sign up for free
                   {/* <span aria-hidden="true"> &rarr;</span> */}
@@ -17,11 +17,16 @@ export default function HeaderBanner() {
             </span>
           </p>
         </div>
-      </div>
-      <div className="absolute inset-x-0 bottom-0 h-1">
-        <div className="h-full bg-gradient-to-r from-green-900 to-green-300"></div>
+        {/* <div className="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:items-start sm:pt-1 sm:pr-2">
+          <button
+            type="button"
+            className="flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
+          >
+            <span className="sr-only">Dismiss</span>
+            <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </button>
+        </div> */}
       </div>
     </div>
   );
 }
-
