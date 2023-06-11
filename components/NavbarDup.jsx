1e3055cb222cba422b65c3a6de4a15cc7322c38e
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-11 18:37:43 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -14,7 +14,10 @@ const navigationAll = [
   { name: 'Blog', href: 'https://blog.kahana.co' },
   { name: 'Solutions', href: 'solutions' },
   { name: 'FAQ', href: 'faq' },
-  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
+  {
+    name: 'Request a demo',
+    href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header',
+  },
   { name: 'Log in', href: 'https://app.kahana.co/login' },
 ];
 
@@ -29,12 +32,7 @@ export default function NavbarDup() {
           <div className="flex items-center justify-between border-b py-2 px-10">
             <Link href="/" aria-label="Home">
               <span className="sr-only">Company</span>
-              <Image
-                className="h-10"
-                src={whiteKahanaLogo}
-                w
-                alt="navbar-logo"
-              />
+              <Image className="h-10" src={whiteKahanaLogo} w alt="navbar-logo" />
             </Link>
             <nav>
               <section className="MOBILE-MENU flex lg:hidden">
@@ -42,86 +40,128 @@ export default function NavbarDup() {
                   className={`HAMBURGER-ICON ${isNavOpen ? 'open' : ''}`}
                   onClick={() => setIsNavOpen((prev) => !prev)}
                 >
-                  <span className="block h-0.5 w-5 bg-gray-600"></span>
-                  <span className="block h-0.5 w-5 bg-gray-600"></span>
+                  <span className="block h-0.5 w-5 bg-gray-600 mb-1"></span>
+                  <span className="block h-0.5 w-5 bg-gray-600 mb-1"></span>
                   <span className="block h-0.5 w-5 bg-gray-600"></span>
                 </div>
 
                 <div className={`MENU-LINK-MOBILE ${isNavOpen ? 'open' : ''}`}>
-                  {navigationAll.map((link) => (
-                    <Link
-                      href={link.href}
-                      key={link.name}
-                      className="text-base font-small text-gray-600 hover:text-gray-800"
-                    >
-                      {link.name}
-                    </Link>
-                  ))}
+                  <ul className="MENU-LINKS">
+                    {navigationAll.map((link) => (
+                      <li key={link.name}>
+                        <Link
+                          href={link.href}
+                          className="text-base font-small text-gray-600 hover:text-gray-800 flex items-center"
+                        >
+                          <span>{link.name}</span>
+                          <svg
+                            className="w-3 h-3 ml-1 text-gray-400"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M19 9l-7 7-7-7"
+                            />
+                          </svg>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </section>
 
               <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
                 {navigationAll.map((link) => (
-                  <Link
-                    href={link.href}
-                    key={link.name}
-                    className="text-base font-small text-gray-600 hover:text-gray-800"
-                  >
-                    {link.name}
-                  </Link>
+                  <li key={link.name}>
+                    <Link
+                      href={link.href}
+                      className="text-base font-small text-gray-600 hover:text-gray-800"
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
                 ))}
               </ul>
             </nav>
           </div>
+          <style>{`
+            .HAMBURGER-ICON {
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              cursor: pointer;
+              height: 20px;
+              width: 20px;
+            }
+
+            .HAMBURGER-ICON.open span:nth-child(1) {
+              transform: rotate(45deg) translate(0, 1px);
+            }
+
+            .HAMBURGER-ICON.open span:nth-child(2) {
+              opacity: 0;
+            }
+
+            .HAMBURGER-ICON.open span:last-child {
+              transform: rotate(-45deg) translate(0, -1px);
+            }
+
+            .MOBILE-MENU {
+              position: relative;
+            }
+
+            .MENU-LINK-MOBILE {
+              display: none;
+              position: absolute;
+              top: 100%;
+              left: 0;
+              padding: 0.5rem 0;
+              background-color: white;
+              box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+              border-radius: 4px;
+            }
+
+            .MENU-LINK-MOBILE.open {
+              display: block;
+            }
+
+            .MENU-LINKS {
+              list-style-type: none;
+              padding: 0;
+              margin: 0;
+            }
+
+            .MENU-LINKS li:not(:last-child) {
+              margin-bottom: 0.5rem;
+            }
+
+            .MENU-LINKS a {
+              display: flex;
+              align-items: center;
+              font-size: 16px;
+              color: #4b5563;
+              transition: color 0.2s;
+            }
+
+            .MENU-LINKS a:hover {
+              color: #1f2937;
+            }
+
+            .MENU-LINKS svg {
+              width: 12px;
+              height: 12px;
+              fill: none;
+              stroke: currentColor;
+              stroke-width: 2;
+              transition: transform 0.2s;
+            }
+          `}</style>
         </nav>
       </header>
-      <style jsx>{`
-        .HAMBURGER-ICON {
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          align-items: center;
-          cursor: pointer;
-          height: 18px;
-        }
-
-        .HAMBURGER-ICON.open span:first-child {
-          transform: rotate(45deg) translate(1px, 1px);
-        }
-
-        .HAMBURGER-ICON.open span:nth-child(2) {
-          opacity: 0;
-        }
-
-        .HAMBURGER-ICON.open span:last-child {
-          transform: rotate(-45deg) translate(0, -1px);
-        }
-
-        .MOBILE-MENU {
-          position: relative;
-        }
-
-        .MENU-LINK-MOBILE {
-          display: none;
-          position: absolute;
-          top: 100%;
-          left: 0;
-          right: 0;
-          padding: 1rem;
-          background-color: white;
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-          border-radius: 4px;
-        }
-
-        .MENU-LINK-MOBILE.open {
-          display: block;
-        }
-
-        .DESKTOP-MENU {
-          flex-grow: 1;
-          justify-content: flex-end;
-        }
-      `}</style>
     </>
   );
 }
