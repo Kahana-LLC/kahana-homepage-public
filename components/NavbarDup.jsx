65522e187b2af474c3d052b9c278eab4fd8eb4f5
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-12 12:21:13 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -1,29 +1,10 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from "react";
-
 import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 import HeaderBanner from './HeaderBanner';
 
-const navigation = [
-  { name: 'Featured Hubs', href: 'explore' },
-  { name: 'About', href: 'about' },
-  { name: 'Pricing', href: 'pricing' },
-  { name: 'Blog', href: 'https://blog.kahana.co' },
-  { name: 'Solutions', href: 'solutions' },
-];
-
-const navigation1 = [
-  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
-  { name: 'Log in', href: 'https://app.kahana.co/login' },
-];
-
-const navigationAll = [
-  ...navigation,
-  ...navigation1,
-];
-
-export default function NavbarDup() {
+const NavigationBar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNavOpen = () => {
@@ -46,9 +27,20 @@ export default function NavbarDup() {
     };
   }, [isNavOpen]);
 
+  const navigationAll = [
+    { name: 'Featured Hubs', href: 'explore' },
+    { name: 'About', href: 'about' },
+    { name: 'Pricing', href: 'pricing' },
+    { name: 'Blog', href: 'https://blog.kahana.co' },
+    { name: 'Solutions', href: 'solutions' },
+    { name: 'FAQ', href: 'faq' },
+    { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
+    { name: 'Log in', href: 'https://app.kahana.co/login' },
+  ];
+
   return (
     <>
-      <HeaderBanner />
+      <HeaderBanner /> {/* Import and use the HeaderBanner component */}
       <header className="backdrop-blur-sm bg-white/90 bg-white mx-auto px-2">
         <nav>
           <div className="flex items-center justify-between border-b py-2 px-10">
@@ -60,43 +52,7 @@ export default function NavbarDup() {
                 alt="navbar-logo"
               />
             </Link>
-
-            <section className="MOBILE-MENU relative flex lg:hidden">
-              <div className="MOBILE-MENU-OVERLAY fixed top-0 left-0 w-full h-screen bg-white z-10" style={{ display: isNavOpen ? 'flex' : 'none' }}>
-                <div className="CROSS-ICON absolute top-4 right-4 px-2 py-2" onClick={toggleNavOpen}>
-                  <svg
-                    className="h-8 w-8 text-gray-600"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <line x1="18" y1="6" x2="6" y2="18" />
-                    <line x1="6" y1="6" x2="18" y2="18" />
-                  </svg>
-                </div>
-                <div className="MOBILE-MENU-CONTENT flex flex-col items-start justify-start h-full p-8">
-                  <ul className="MENU-LINK-MOBILE-OPEN space-y-4 text-lg">
-                    {navigationAll.map((link) => (
-                      <li key={link.name}>
-                        <Link href={link.href} className="text-gray-600 hover:text-gray-800">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-
-              <div className="HAMBURGER-ICON space-y-2" onClick={toggleNavOpen} style={{ display: isNavOpen ? 'none' : 'block' }}>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              </div>
-            </section>
-
+            {/* Desktop Menu */}
             <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
               {navigationAll.map((link) => (
                 <li key={link.name}>
@@ -106,54 +62,64 @@ export default function NavbarDup() {
                 </li>
               ))}
             </ul>
+            {/* Mobile Menu */}
+            <div className="lg:hidden">
+              <button
+                type="button"
+                onClick={toggleNavOpen}
+                className="text-gray-600 hover:text-gray-800 focus:outline-none"
+              >
+                {isNavOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
+          {/* Mobile Menu Overlay */}
+          {isNavOpen && (
+            <div className="fixed inset-0 flex items-center justify-center bg-white/90">
+              <div className="bg-white px-8 py-4 space-y-4">
+                {navigationAll.map((link) => (
+                  <Link href={link.href} key={link.name} className="block text-xl font-medium text-gray-600 hover:text-gray-800">
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
         </nav>
       </header>
-      <style jsx>{`
-        .hideMenuNav {
-          display: none;
-        }
-        .showMenuNav {
-          display: block;
-          position: absolute;
-          width: 100%;
-          height: 100vh;
-          top: 0;
-          left: 0;
-          background: white;
-          z-index: 10;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-evenly;
-          align-items: center;
-        }
-
-        .MOBILE-MENU-OVERLAY {
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-start;
-          align-items: flex-start;
-          padding-top: 2rem;
-        }
-
-        .CROSS-ICON {
-          cursor: pointer;
-        }
-
-        .MOBILE-MENU-CONTENT {
-          width: 100%;
-        }
-
-        .MENU-LINK-MOBILE-OPEN {
-          list-style: none;
-          padding: 0;
-          margin: 0;
-        }
-      `}</style>
     </>
   );
-}
+};
+
+export default NavigationBar;
+
