d19551241cadb3bda84564acc6586e0c96ce467b
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-12 13:01:54 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -1,10 +1,29 @@
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from "react";
+
 import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 import HeaderBanner from './HeaderBanner';
 
-const NavigationBar = () => {
+const navigation = [
+  { name: 'Featured Hubs', href: 'explore' },
+  { name: 'About', href: 'about' },
+  { name: 'Pricing', href: 'pricing' },
+  { name: 'Blog', href: 'https://blog.kahana.co' },
+  { name: 'Solutions', href: 'solutions' },
+];
+
+const navigation1 = [
+  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
+  { name: 'Log in', href: 'https://app.kahana.co/login' },
+];
+
+const navigationAll = [
+  ...navigation,
+  ...navigation1,
+];
+
+export default function NavbarDup() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNavOpen = () => {
@@ -14,36 +33,23 @@ const NavigationBar = () => {
   useEffect(() => {
     const bodyScrollLock = () => {
       if (isNavOpen) {
-        document.documentElement.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';
       } else {
-        document.documentElement.style.overflow = 'auto';
+        document.body.style.overflow = 'auto';
       }
     };
 
     bodyScrollLock();
 
     return () => {
-      document.documentElement.style.overflow = 'auto';
+      document.body.style.overflow = 'auto';
     };
   }, [isNavOpen]);
 
-  const navigationAll = [
-    { name: 'Featured Hubs', href: 'explore' },
-    { name: 'About', href: 'about' },
-    { name: 'Pricing', href: 'pricing' },
-    { name: 'Blog', href: 'https://blog.kahana.co' },
-    { name: 'Solutions', href: 'solutions' },
-    { name: 'FAQ', href: 'faq' },
-    { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_header' },
-    { name: 'Log in', href: 'https://app.kahana.co/login' },
-  ];
-
   return (
     <>
-      <div className="absolute w-full z-50">
-        <HeaderBanner /> {/* Import and use the HeaderBanner component */}
-      </div>
-      <header className="backdrop-blur-sm bg-white/90 bg-white mx-auto px-2 relative z-50">
+      <HeaderBanner />
+      <header className="backdrop-blur-sm bg-white/90 bg-white mx-auto px-2">
         <nav>
           <div className="flex items-center justify-between border-b py-2 px-10">
             <Link href="/" aria-label="Home">
@@ -54,8 +60,44 @@ const NavigationBar = () => {
                 alt="navbar-logo"
               />
             </Link>
-            {/* Desktop Menu */}
-            <ul className="hidden space-x-8 lg:flex">
+
+            <section className="MOBILE-MENU relative flex lg:hidden">
+              <div className="MOBILE-MENU-OVERLAY fixed top-0 left-0 w-full h-screen bg-white z-10" style={{ display: isNavOpen ? 'flex' : 'none' }}>
+                <div className="CROSS-ICON absolute top-4 right-4 px-2 py-2" onClick={toggleNavOpen}>
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <div className="MOBILE-MENU-CONTENT flex flex-col items-start justify-start h-full p-8">
+                  <ul className="MENU-LINK-MOBILE-OPEN space-y-4 text-lg">
+                    {navigationAll.map((link) => (
+                      <li key={link.name}>
+                        <Link href={link.href} className="text-gray-600 hover:text-gray-800">
+                          {link.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+
+              <div className="HAMBURGER-ICON space-y-2" onClick={toggleNavOpen} style={{ display: isNavOpen ? 'none' : 'block' }}>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              </div>
+            </section>
+
+            <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
               {navigationAll.map((link) => (
                 <li key={link.name}>
                   <Link href={link.href} className="text-base font-small text-gray-600 hover:text-gray-800">
@@ -64,65 +106,54 @@ const NavigationBar = () => {
                 </li>
               ))}
             </ul>
-            {/* Mobile Menu */}
-            <div className="lg:hidden">
-              <button
-                type="button"
-                onClick={toggleNavOpen}
-                className="text-gray-600 hover:text-gray-800 focus:outline-none"
-              >
-                {isNavOpen ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
           </div>
-          {/* Mobile Menu Overlay */}
-          {isNavOpen && (
-            <div className="fixed inset-0 bg-white z-40">
-              <div className="px-8 py-4 space-y-4 mt-20">
-                {navigationAll.map((link) => (
-                  <Link href={link.href} key={link.name} className="block text-xl font-medium text-gray-600 hover:text-gray-800">
-                    {link.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          )}
         </nav>
       </header>
+      <style jsx>{`
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: white;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+
+        .MOBILE-MENU-OVERLAY {
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          padding-top: 2rem;
+        }
+
+        .CROSS-ICON {
+          cursor: pointer;
+        }
+
+        .MOBILE-MENU-CONTENT {
+          width: 100%;
+        }
+
+        .MENU-LINK-MOBILE-OPEN {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
     </>
   );
-};
-
-export default NavigationBar;
-
-
+}
