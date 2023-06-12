dbbf19d8f369b85dfd30fb2f889d77b3f697407d
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-06-12 00:21:03 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+
 import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 import HeaderBanner from './HeaderBanner';
 
@@ -17,7 +18,10 @@ const navigation1 = [
   { name: 'Log in', href: 'https://app.kahana.co/login' },
 ];
 
-const navigationAll = [...navigation, ...navigation1];
+const navigationAll = [
+  ...navigation,
+  ...navigation1,
+];
 
 export default function NavbarDup() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -50,64 +54,106 @@ export default function NavbarDup() {
           <div className="flex items-center justify-between border-b py-2 px-10">
             <Link href="/" aria-label="Home">
               <span className="sr-only">Company</span>
-              <Image className="h-10" src={whiteKahanaLogo} alt="navbar-logo" />
+              <Image
+                className="h-10"
+                src={whiteKahanaLogo}
+                alt="navbar-logo"
+              />
             </Link>
-            <div className="flex items-center space-x-4 md:space-x-6">
-              <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
-                {navigationAll.map((link) => (
-                  <li key={link.name}>
-                    <Link href={link.href} passHref>
-                      <a className="text-base font-small text-gray-600 hover:text-gray-800">
-                        {link.name}
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <button
-                type="button"
-                className="md:hidden focus:outline-none"
-                onClick={toggleNavOpen}
-              >
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  {isNavOpen ? (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  ) : (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  )}
-                </svg>
-              </button>
-            </div>
-          </div>
-          {isNavOpen && (
-            <div className="md:hidden bg-white absolute inset-x-0 top-16 z-20">
-              <div className="px-4 py-2 space-y-4">
-                {navigationAll.map((item) => (
-                  <Link key={item.href} href={item.href}>
-                    <a className="block text-gray-700 hover:text-gray-900">{item.name}</a>
-                  </Link>
-                ))}
+
+            <section className="MOBILE-MENU relative flex lg:hidden">
+              <div className="MOBILE-MENU-OVERLAY fixed top-0 left-0 w-full h-screen bg-white z-10" style={{ display: isNavOpen ? 'flex' : 'none' }}>
+                <div className="CROSS-ICON absolute top-4 right-4 px-2 py-2" onClick={toggleNavOpen}>
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <div className="MOBILE-MENU-CONTENT flex flex-col items-start justify-start h-full p-8">
+                  <ul className="MENU-LINK-MOBILE-OPEN space-y-4 text-lg">
+                    {navigationAll.map((link) => (
+                      <li key={link.name}>
+                        <Link href={link.href} className="text-gray-600 hover:text-gray-800">
+                          {link.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-            </div>
-          )}
+
+              <div className="HAMBURGER-ICON space-y-2" onClick={toggleNavOpen} style={{ display: isNavOpen ? 'none' : 'block' }}>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              </div>
+            </section>
+
+            <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+              {navigationAll.map((link) => (
+                <li key={link.name}>
+                  <Link href={link.href} className="text-base font-small text-gray-600 hover:text-gray-800">
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </nav>
       </header>
+      <style jsx>{`
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: white;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+
+        .MOBILE-MENU-OVERLAY {
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          padding-top: 2rem;
+        }
+
+        .CROSS-ICON {
+          cursor: pointer;
+        }
+
+        .MOBILE-MENU-CONTENT {
+          width: 100%;
+        }
+
+        .MENU-LINK-MOBILE-OPEN {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
     </>
   );
 }
