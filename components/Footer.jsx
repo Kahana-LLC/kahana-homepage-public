22bbc57861b14fa9e61a62f3e2898d81ecf97e5e
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-07-24 12:57:31 -0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,6 +3,10 @@ import Link from 'next/link'
 import Image from 'next/image';
 import grayKahanaLogo from '../assets/kahana_logo_wide_gray.svg';
 
+const CustomLoader = ({ src, width, quality }) => {
+  return `https://assets.capterra.com/badge/14c0b2030cba21f2961e6c6aed65228b.svg?v=2289101&p=342047`;
+};
+
 const navigation = {
   important: [
     { name: 'Explore', href: '/explore' },
@@ -78,7 +82,8 @@ export default function Footer() {
             <div className="sf-root" data-id="3652674" data-badge="heart-badge-white" data-variant-id="sf" style={{ width: '75px' }}></div>
             <a href="https://www.capterra.com/reviews/342047/Kahana?utm_source=vendor&utm_medium=badge&utm_campaign=capterra_reviews_badge">
               <Image
-                src="https://assets.capterra.com/badge/14c0b2030cba21f2961e6c6aed65228b.svg?v=2289101&p=342047"
+                loader={CustomLoader}
+                src="capterra-badge"
                 alt="Capterra Badge"
                 width={100} // Adjust the width here as per your requirement
                 height={40} // Adjust the height here as per your requirement
