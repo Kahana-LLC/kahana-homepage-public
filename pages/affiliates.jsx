fbd8ebbb862861f021a8f85cdbf9f37362131dec
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-10 17:02:48 -0500

    Update affiliates.jsx

--- pages/affiliates.jsx
+++ pages/affiliates.jsx
@@ -63,6 +63,11 @@ const faqs = [
 
 export default function AffiliateProgramPage() {
   const [showImage, setShowImage] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -79,7 +84,20 @@ export default function AffiliateProgramPage() {
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
         />
-        {/* Google Tag Manager script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KQHFL9605P"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KQHFL9605P');
+            `,
+          }}
+        />
       </Head>
       <div>
         <div className="sticky top-0">
