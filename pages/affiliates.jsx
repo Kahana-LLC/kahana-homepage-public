a466dcc76d66b5cd91369187033669f8c91a9627
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-10 16:43:39 -0500

    Update affiliates.jsx

--- pages/affiliates.jsx
+++ pages/affiliates.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Footer from '../components/Footer';
 import NavbarDup from '../components/NavbarDup';
