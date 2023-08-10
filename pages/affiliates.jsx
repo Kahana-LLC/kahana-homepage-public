b7e676e4ac1ff93af76fd68a25799551acb4730b
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-10 15:26:43 -0500

    Update affiliates.jsx

--- pages/affiliates.jsx
+++ pages/affiliates.jsx
@@ -16,7 +16,7 @@ const features = [
     title: 'Quality and Reputation',
     description: (
       <span>
-        As an affiliate, you should be cautious about the products you endorse. We are proud to be a software platform that is used daily by experts, teams, and organizations across the world. Simply put, it\'s in our DNA to continue to seek ways to improve our platform from our users and customers, so that as an affiliate, you know you will be sharing a platform that is hungry to deliver substantial value in the short-term and long-term for users. You can see reviews from our happy customers on{' '}
+        As an affiliate, you should be cautious about the products you endorse. We are proud to be a software platform that is used daily by experts, teams, and organizations across the world. Simply put, it&apos;s in our DNA to continue to seek ways to improve our platform from our users and customers, so that as an affiliate, you know you will be sharing a platform that is hungry to deliver substantial value in the short-term and long-term for users. You can see reviews from our happy customers on{' '}
         <a href="https://www.capterra.com/p/10009805/Kahana/" className="text-[#038270] font-medium">
           Capterra
         </a>{' '}
