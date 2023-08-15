ef48587fc1ecfc069aef5911d989d7188b5ec06e
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-15 15:18:44 -0500

    Update resources.jsx

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -81,139 +81,80 @@ const ResourcesPage = () => {
         />
       </Head>
 
-        <div className="sticky top-0 z-50">
-          <NavbarDup />
-        </div>
+      <div className="sticky top-0 z-50">
+        <NavbarDup />
+      </div>
 
-      <style jsx>{`
-        /* Add your global styles and other styles here */
-
-        /* Specific styles for the resources page */
-        .header-section {
-          text-align: center;
-          padding: 80px 0;
-        }
-
-        .header-text {
-          max-width: 600px;
-          margin: 0 auto;
-        }
-
-        .resources-section {
-          text-align: center;
-          padding: 60px 0;
-        }
-
-        .clickable-tiles-section {
-          background-color: #f5f5f5;
-          padding: 80px 0;
-        }
-
-        .tile-row {
-          display: flex;
-          justify-content: center;
-          gap: 20px;
-          flex-wrap: wrap;
-        }
-
-        .tile {
-          background-color: #ffffff;
-          padding: 20px;
-          border-radius: 8px;
-          box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-          flex: 1;
-          text-align: center;
-        }
-
-        .cta-section {
-          text-align: center;
-          padding: 60px 0;
-        }
-
-        .cta-section button {
-          background-color: #007bff;
-          color: #ffffff;
-          padding: 10px 20px;
-          border: none;
-          border-radius: 4px;
-          font-size: 18px;
-          cursor: pointer;
-        }
-
-        .links-section {
-          text-align: center;
-          padding: 80px 0;
-        }
-
-        .link-row {
-          display: flex;
-          justify-content: center;
-          gap: 20px;
-          flex-wrap: wrap;
-        }
-
-        .link {
-          flex: 1;
-          background-color: #ffffff;
-          padding: 20px;
-          border-radius: 8px;
-          box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-          text-align: center;
-        }
-      `}</style>
-
-      {/* Header Section */}
       <section className="header-section">
         <div className="header-text">
-          <h1>Top Resources to Monetize Your Knowledge</h1>
-          <p>A collection of actionable resources to help you monetize your best knowledge assets.</p>
+          <h1 className="text-3xl font-bold mb-4">
+            Top Resources to Monetize Your Knowledge
+          </h1>
+          <p className="text-gray-600">
+            A collection of actionable resources to help you monetize your best knowledge assets.
+          </p>
         </div>
       </section>
-      
-      {/* Clickable Tiles Section */}
-      <section className="clickable-tiles-section">
-        <h2>📍 Guides</h2>
-        <p>Learning and material to help you monetize.</p>
-        <div className="tile-row">
+
+      <section className="clickable-tiles-section bg-gray-100 py-12">
+        <h2 className="text-2xl font-semibold mb-2">📍 Guides</h2>
+        <p className="text-gray-600">
+          Step-by-step roadmaps to help you monetize.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
           {clickableTiles.map((tile, index) => (
-            <div key={index} className="tile">
-              <h3>{tile.title}</h3>
-              <p>{tile.description}</p>
+            <div
+              key={index}
+              className="bg-white p-6 rounded-lg shadow-md text-center"
+            >
+              <h3 className="text-xl font-semibold mb-2">{tile.title}</h3>
+              <p className="text-gray-600">{tile.description}</p>
             </div>
           ))}
         </div>
       </section>
 
-      {/* Call to Action Section */}
-      <section className="cta-section">
-        <h2>Want to start monetizing your knowledge?</h2>
-        <p>Start charging for acccess to your best assets in minutes with Kahana.</p>
-        <button>
+      <section className="cta-section bg-gray-100 py-12">
+        <h2 className="text-2xl font-semibold mb-2">
+          Want to start monetizing your knowledge?
+        </h2>
+        <p className="text-gray-600">
+          Start charging for access to your best assets in minutes with Kahana.
+        </p>
+        <button className="bg-black text-white px-6 py-3 rounded-md mt-4">
           <a href="https://app.kahana.co/signup">Get Started</a>
         </button>
       </section>
 
-      {/* Links Section */}
-      <section className="links-section">
-        <div className="link-row">
+      <section className="links-section bg-gray-100 py-12">
+        <h2 className="text-2xl font-semibold mb-2">More Collections</h2>
+        <p className="text-gray-600">
+          Discover additional resources for your success.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
           {links.map((link, index) => (
-            <div key={index} className="link">
-              <h3>{link.title}</h3>
-              <p>{link.description}</p>
+            <div
+              key={index}
+              className="bg-white p-6 rounded-lg shadow-md text-center"
+            >
+              <h3 className="text-xl font-semibold mb-2">{link.title}</h3>
+              <p className="text-gray-600">{link.description}</p>
             </div>
           ))}
         </div>
       </section>
 
-      {/* FAQ Section */}
-      <section className="py-16 px-4 sm:px-6 lg:px-8 bg-white">
+      <section className="faq-section bg-white py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <h2 className="text-3xl font-bold mb-8 text-gray-900 text-center">
             Most frequently asked questions
           </h2>
           <div className="space-y-4">
             {faqs.map((faq, index) => (
-              <div key={index} className="border border-gray-200 rounded-lg">
+              <div
+                key={index}
+                className="border border-gray-200 rounded-lg"
+              >
                 <button
                   className="w-full flex justify-between items-center p-4 focus:outline-none"
                   onClick={() => toggleAccordion(index)}
