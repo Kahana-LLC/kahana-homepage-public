f9fb40c05f7cd4cb16327363fc71e31ee35ca2c4
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 02:34:17 -0500

    Update NavbarResources.jsx

--- components/NavbarResources.jsx
+++ components/NavbarResources.jsx
@@ -3,36 +3,10 @@ import Link from 'next/link';
 
 //images
 import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
-import HeaderBanner from './HeaderBanner';
-
-//navigation items
-const navigation = [
-  { name: 'Featured Hubs', href: 'explore' },
-  { name: 'About', href: 'about' },
-  { name: 'Pricing', href: 'pricing' },
-  { name: 'Blog', href: ' https://blog.kahana.co' },
-  { name: 'Solutions', href: 'solutions' },
-];
-
-const navigation1 = [
-  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_source=xxxxx&utm_medium=xxxxx&utm_campaign=xxxxx&utm_term=xxxxx&utm_content=xxxxx' },
-  { name: 'Log in', href: 'https://app.kahana.co/login' },
-];
-
-const navigationAll = [
-  { name: 'Featured Hubs', href: 'explore' },
-  { name: 'About', href: 'about' },
-  { name: 'Pricing', href: 'pricing' },
-  { name: 'Blog', href: ' https://blog.kahana.co' },
-  { name: 'Solutions', href: 'solutions' },
-  { name: 'Request a demo', href: 'https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_source=xxxxx&utm_medium=xxxxx&utm_campaign=xxxxx&utm_term=xxxxx&utm_content=xxxxx' },
-  { name: 'Log in', href: 'https://app.kahana.co/login' },
-];
 
 function Navbar() {
   return (
     <>
-      <HeaderBanner />
       <header className="backdrop-blur-sm bg-white/90 bg-white  mx-auto px-2">
         <nav>
           <div className="flex w-full items-center justify-between border-b  py-2 px-10">
@@ -48,45 +22,15 @@ function Navbar() {
                   alt="navbar-logo"
                 />
               </Link>
-              <div className="ml-10 hidden space-x-8 lg:block">
-                {navigation.map((link) => (
-                  <Link
-                    href={link.href}
-                    key={link.name}
-                    className="text-base font-small text-gray-600  hover:text-gray-800"
-                  >
-                    {link.name}
-                  </Link>
-                ))}
-              </div>
             </div>
             <div className="ml-10 space-x-4 ">
               <a
-                href="https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_source=xxxxx&utm_medium=xxxxx&utm_campaign=xxxxx&utm_term=xxxxx&utm_content=xxxxx"
+                href="https://kahana.co?utm_source=resources"
                 className="inline-block justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] drop-shadow-2xl"
               >
-                Request a demo
-              </a>
-              <a
-                href="https://app.kahana.co/login"
-                className="inline-block justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] drop-shadow-2xl"
-              >
-                Log in
+                Learn more
               </a>
             </div>
-          </div>
-          {/* mobile menu */}
-
-          <div className="flex flex-nowrap justify-start space-x-2 py-4 lg:hidden">
-            {navigation.map((link) => (
-              <Link
-                href={link.href}
-                key={link.name}
-                className="text-xs text-slate-600  hover:text-slate-900"
-              >
-                {link.name}
-              </Link>
-            ))}
           </div>
         </nav>
       </header>
