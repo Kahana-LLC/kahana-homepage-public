702fb7e725aa2b2013641178a47ebe48df3adc90
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 01:17:47 -0500

    Create NotionResources.jsx

---
+++ components/NotionResources.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+
+const NotionResources = () => {
+  const links = [
+    {
+      title: 'Ultimate Guide to Monetize Notion',
+      link: '/resources/ultimate-guide-to-monetize-notion'
+    },
+    {
+      title: 'Helpful Link 2',
+      link: '/resources/helpful-link-2'
+    },
+    {
+      title: 'Informative Link 3',
+      link: '/resources/informative-link-3'
+    },
+    // Add more links as needed
+  ];
+
+  return (
+    <section className="bg-gray-100 py-10">
+      <div className="max-w-7xl mx-auto text-center">
+        <h2 className="text-2xl font-semibold mb-2">Resources to Monetize Notion</h2>
+        <p className="text-gray-600">
+          Learn how to securely monetize your best knowledge, information, and templates that live in Notion.
+        </p>
+        <div className="flex flex-wrap justify-center mt-6">
+          {links.map((link, index) => (
+            <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-4">
+              <Link href={link.link}>
+                <a className="block bg-white p-6 rounded-lg shadow-md text-center">
+                  {link.title}
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotionResources;
