d88359aa178835c79abdd02fac046fddb5722226
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 03:01:18 -0500

    Update DigitalProductsResources.jsx

--- components/DigitalProductsResources.jsx
+++ components/DigitalProductsResources.jsx
@@ -20,9 +20,9 @@ const DigitalProductsResources = () => {
 
   return (
     <section className="bg-gray-100 py-10">
-      <div className="max-w-7xl mx-auto px-4 md:px-8 text-center">
-        <h2 className="text-2xl font-semibold mb-2">Resources to Sell Knowledge-Based Digital Products</h2>
-        <p className="text-gray-600 mb-4 md:w-3/4 mx-auto">
+      <div className="max-w-7xl mx-auto text-center">
+        <h2 className="text-2xl font-semibold mb-2 px-2">Resources to Sell Knowledge-Based Digital Products</h2>
+        <p className=""text-gray-600 px-2">
           Learn best practices for monetizing your knowledge with digital products.
         </p>
         <div className="flex flex-wrap justify-center mt-6">
