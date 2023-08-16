9d901ce76d885eee54c31d348be689fc442b36f4
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 02:22:57 -0500

    Update resources.jsx

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -18,16 +18,16 @@ const ResourcesPage = () => {
 
   const clickableTiles = [
     {
-      title: 'I want to learn more about selling knowledge-based digital products in general',
-      link: '#digital-products',
+      title: 'Selling knowledge-based digital products in general',
+      target: 'digital-products',
     },
     {
-      title: 'I want to learn how I can monetize my knowledge and assets in Google Drive',
-      link: '#google-drive',
+      title: 'Monetizing Google Drive',
+      target: 'google-drive',
     },
     {
-      title: 'I want to learn how I can monetize my knowledge, information, and templates in Notion',
-      link: '#notion',
+      title: 'Monetizing Notion',
+      target: 'notion',
     },
   ];
   
@@ -90,18 +90,18 @@ const ResourcesPage = () => {
       {/* Clickable Tiles Section */}
       <section className="bg-gray-100 py-10">
         <div className="max-w-7xl mx-auto text-center">
-          <h2 className="text-2xl font-semibold mb-2">Which of the following best describes you?</h2>
+          <h2 className="text-2xl font-semibold mb-2">Which of the following would you like to learn more about?</h2>
           {/* <p className="text-gray-600">
-            Explore all of our Learning and material to help you monetize.
+            Learning and material to help you monetize.
           </p> */}
           <div className="flex flex-wrap justify-center mt-6 space-y-4 sm:space-y-0 sm:gap-4">
             {clickableTiles.map((tile, index) => (
               <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-2">
                 <a
-                  href={tile.link}
+                  href={`#${tile.target}`}
                   onClick={(e) => {
                     e.preventDefault();
-                    const targetId = tile.link.substring(1); // Remove the '#'
+                    const targetId = tile.target;
                     const targetElement = document.getElementById(targetId);
                     if (targetElement) {
                       window.scrollTo({
