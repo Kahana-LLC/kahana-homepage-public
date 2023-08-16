247b230ff290f85fb5b4c23ec0c57129834c9459
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 01:45:05 -0500

    Update ultimate-guide-to-monetize-notion.jsx

--- pages/resources/ultimate-guide-to-monetize-notion.jsx
+++ pages/resources/ultimate-guide-to-monetize-notion.jsx
@@ -7,7 +7,7 @@ import ResourcesCalltoAction from '../../components/ResourcesCalltoAction';
 
 const Page = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-gray-100">
       <Head>
         <title>Ultimate Guide to Monetize Notion</title>
         <meta
