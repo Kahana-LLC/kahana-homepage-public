56255457fd66b6cd5abfe60091edf57ad7aa6d40
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 01:34:20 -0500

    Update ultimate-guide-to-monetize-notion.jsx

--- pages/resources/ultimate-guide-to-monetize-notion.jsx
+++ pages/resources/ultimate-guide-to-monetize-notion.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Footer from '../../components/Footer';
 import NavbarDup from '../../components/NavbarDup';
 import Image from 'next/image';
-import ResourcesCallToAction from '../../ResourcesCallToAction';
+import ResourcesCalltoAction from '../../components/ResourcesCalltoAction';
 
 const Page = () => {
   return (
