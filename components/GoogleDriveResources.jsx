08eee1c2a8e99d0417f5163e802bcc010999440b
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-16 06:01:14 -0500

    Update GoogleDriveResources.jsx

--- components/GoogleDriveResources.jsx
+++ components/GoogleDriveResources.jsx
@@ -8,14 +8,53 @@ const GoogleDriveResources = () => {
       link: '/resources/how-to-make-money-with-google-docs'
     },
     {
-      title: 'Helpful Link 2',
-      link: '/resources/helpful-link-2'
+      title: 'How to make money with Google Drive',
+      link: '/resources/how-to-make-money-with-google-drive'
     },
     {
-      title: 'Informative Link 3',
-      link: '/resources/informative-link-3'
+      title: 'How to sell products hosted on Google Drive',
+      link: '/resources/how-to-sell-products-hosted-on-google-drive'
     },
-    // Add more links as needed
+    {
+      title: 'How to sell digital products online with Google Drive',
+      link: '/resources/how-to-sell-digital-products-online-with-google-drive'
+    },
+    {
+      title: 'How to sell a Google Doc',
+      link: '/resources/how-to-sell-a-google-doc'
+    },
+    {
+      title: 'How to make money online with Google Sheets',
+      link: '/resources/how-to-make-money-online-with-google-sheets'
+    },
+    {
+      title: 'How to monetize a spreadsheet',
+      link: '/resources/how-to-monetize-a-spreadsheet'
+    },
+    {
+      title: 'How to charge for spreadsheets',
+      link: '/resources/how-to-charge-for-spreadsheets'
+    },
+    {
+      title: 'How to sell Excel or Google spreadsheets',
+      link: '/resources/how-to-sell-excel-or-google-spreadsheets'
+    },
+    {
+      title: 'Where to sell Google Sheets',
+      link: '/resources/where-to-sell-google-sheets'
+    },
+    {
+      title: 'Best-selling spreadsheets',
+      link: '/resources/best-selling-spreadsheets'
+    },
+    {
+      title: 'Best ways to make money selling Google Sheets',
+      link: '/resources/best-ways-to-make-money-selling-google-sheets'
+    },
+    {
+      title: 'How to sell spreadsheets online',
+      link: '/resources/how-to-sell-spreadsheets-online'
+    },
   ];
 
   return (
