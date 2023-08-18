acd4c6811537077382d09ab855073eab2f3b6223
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-18 15:10:27 -0500

    Create recurring-revenue.jsx

---
+++ pages/product/recurring-revenue.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Head from 'next/head';
+import Footer from '../../components/Footer';
+import NavbarDup from '../../components/NavbarDup';
+import Image from 'next/image';
+
+const WikisPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Turn your knowledge into recurring revenue</title>
+        <meta
+          name="Kahana"
+          content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KQHFL9605P"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KQHFL9605P');
+            `,
+          }}
+        />
+      </Head>
+
+      <div className="sticky top-0 z-50">
+        <NavbarDup />
+      </div>
+
+      {/* Hero section */}
+      <section className="py-16 md:py-18">
+        <div className="container mx-auto px-4 md:px-8 text-center">
+          <h1 className="text-4xl font-bold mb-4 md:px-12 lg:px-24">
+            Turn your knowledge into recurring revenue
+          </h1>
+          <p className="text-lg mb-8 md:px-12 lg:px-24">
+            Build dynamic hubs of all your templates, insights, reserach, methodologies, and best practices. It's like charging for access to your brain. 
+          </p>
+          <a
+            href="https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=wikis_page"
+            className="bg-[#3B675E] text-white py-2 px-6 rounded-md text-center inline-block"
+          >
+            Get Kahana free
+          </a>
+        </div>
+      </section>
+
+      {/* Features section */}
+      <section className="bg-gray-100 py-20">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+            <div className="md:col-span-1">
+              <Image
+                src="/wiki-feature-1.png"
+                alt="Feature 1"
+                width={360}
+                height={240}
+              />
+            </div>
+            <div className="md:col-span-1">
+              <h2 className="text-3xl font-semibold mb-4">
+                Collaborative Wikis
+              </h2>
+              <p className="text-lg mb-6">
+                Create and collaborate on wikis with your team. Share knowledge, documentation, and best practices.
+              </p>
+              <p className="text-lg">
+                <a href="#" className="text-blue-500 underline">
+                  Learn more
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* More feature sections can be added similarly */}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default WikisPage;
