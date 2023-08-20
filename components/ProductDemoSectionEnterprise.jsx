c17226334edc59dbda002f9cb9a75759a1b7387b
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 11:35:50 -0500

    Update ProductDemoSectionEnterprise.jsx

--- components/ProductDemoSectionEnterprise.jsx
+++ components/ProductDemoSectionEnterprise.jsx
@@ -104,11 +104,30 @@ export default function ProductDemoSection() {
                   ))}
                 </Tab.List>
               </div>
-              {tabOrientation === 'vertical' && (
-                <div className="hidden lg:block lg:col-span-7">
-                  {features.map((feature) => (
+              <Tab.Panels className="lg:col-span-7">
+                {tabOrientation === 'vertical' ? (
+                  features.map((feature) => (
                     <Tab.Panel key={feature.title} unmount={false}>
                       <div className="relative sm:px-6">
+                        <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                          {feature.description}
+                        </p>
+                        <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                          <Image
+                            className="w-full"
+                            src={feature.image}
+                            alt=""
+                            priority
+                            sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+                          />
+                        </div>
+                      </div>
+                    </Tab.Panel>
+                  ))
+                ) : (
+                  features.map((feature) => (
+                    <Tab.Panel key={feature.title} unmount={false}>
+                      <div className="relative sm:px-6 lg:hidden">
                         <div className="absolute -inset-x-4 top-[-6.5rem] bottom-[-4.25rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
                         <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
                           {feature.description}
@@ -124,9 +143,9 @@ export default function ProductDemoSection() {
                         />
                       </div>
                     </Tab.Panel>
-                  ))}
-                </div>
-              )}
+                  ))
+                )}
+              </Tab.Panels>
             </>
           )}
         </Tab.Group>
