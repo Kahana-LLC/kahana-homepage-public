50097dd8eae72c284003f17bef31cbb1352dbc75
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 15:31:51 -0500

    Update ProductDemoSectionEnterprise.jsx

--- components/ProductDemoSectionEnterprise.jsx
+++ components/ProductDemoSectionEnterprise.jsx
@@ -93,7 +93,7 @@ export default function ProductDemoSection() {
           <div className="mt-16 space-y-12">
             {features.map((feature) => (
               <div key={feature.title} className="space-y-6 flex flex-col lg:flex-row lg:space-x-6">
-                <div className="max-w-[45rem] mx-auto lg:w-1/2 rounded-lg bg-gray-100 p-4">
+                <div className="max-w-[45rem] mx-auto lg:w-1/2">
                   <h2 className="text-2xl font-semibold text-black">
                     {feature.title}
                   </h2>
@@ -105,7 +105,7 @@ export default function ProductDemoSection() {
                     </a>
                   </p>
                 </div>
-                <div className="max-w-[45rem] mx-auto lg:w-1/2 rounded-lg bg-gray-100 p-4">
+                <div className="max-w-[45rem] mx-auto lg:w-1/2">
                   <Image
                     className="w-full"
                     src={feature.image}
