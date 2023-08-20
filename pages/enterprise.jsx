914c90c3fe8aea861afed2c85e63aeec0642cf83
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 18:17:11 -0500

    Update enterprise.jsx

--- pages/enterprise.jsx
+++ pages/enterprise.jsx
@@ -125,7 +125,7 @@ const EnterprisePage = () => {
       {/* Testimonial section */}
       <section className="py-16 md:py-18">
         <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-8">What Our Clients Say</h2>
+          <h2 className="text-3xl font-bold mb-8">What our clients say</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4 md:px-0">
             {/* Add "px-4 md:px-0" to the grid container */}
             <div className="bg-white p-6 rounded-lg shadow-md">
