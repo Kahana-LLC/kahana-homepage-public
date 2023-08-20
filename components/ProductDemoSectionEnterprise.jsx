62d1246e8616096d685d40f42e3bc23ff56d08ce
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 12:14:33 -0500

    Update ProductDemoSectionEnterprise.jsx

--- components/ProductDemoSectionEnterprise.jsx
+++ components/ProductDemoSectionEnterprise.jsx
@@ -1,3 +1,4 @@
+import { Tab } from '@headlessui/react';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
@@ -29,13 +30,13 @@ const features = [
 ];
 
 export default function ProductDemoSection() {
-  let [tabOrientation, setTabOrientation] = useState('horizontal');
+  let [tabOrientation, setTabOrientation] = useState('vertical'); // Change the initial value
 
   useEffect(() => {
     let lgMediaQuery = window.matchMedia('(min-width: 1024px)');
 
     function onMediaQueryChange({ matches }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal');
+      setTabOrientation(matches ? 'horizontal' : 'vertical'); // Switch the values here
     }
 
     onMediaQueryChange(lgMediaQuery);
@@ -58,38 +59,107 @@ export default function ProductDemoSection() {
             Built for Creators and Experts
           </h1>
           <p className="mt-6 text-xl tracking-tight text-white">
-            You have years of valuable information sitting in your brain and resources gathering dust in a (digital)
-            folder. Kahana helps you turn your collective knowledge into hubs that generate income for you.
+            You have years of valuable information sitting in your brain and
+            resources gathering dust in a (digital) folder. Kahana helps you
+            turn your collective knowledge into hubs that generate income for
+            you.
           </p>
         </div>
         {tabOrientation === 'vertical' ? (
-          <div className="mt-6">
-            {features.map((feature, index) => (
-              <div key={index} className="mb-8">
-                <h3 className="text-2xl font-bold mb-2">{feature.title}</h3>
-                <p className="text-gray-400">{feature.description}</p>
-                <div className="mt-4">
-                  <Image src={feature.image} alt={feature.title} width={200} height={200} />
+          <div className="mt-16 space-y-12">
+            {features.map((feature) => (
+              <div key={feature.title} className="space-y-6">
+                <div className="max-w-[45rem] mx-auto">
+                  <h2 className="text-2xl font-semibold text-white">
+                    {feature.title}
+                  </h2>
+                  <p className="mt-2 text-lg text-white">
+                    {feature.description}
+                  </p>
+                </div>
+                <div className="max-w-[45rem] mx-auto">
+                  <Image
+                    className="w-full"
+                    src={feature.image}
+                    alt=""
+                    priority
+                    sizes="(min-width: 1024px) 45rem, (min-width: 640px) 100vw, 90vw"
+                  />
                 </div>
               </div>
             ))}
           </div>
         ) : (
-          <div className="mt-16 grid grid-cols-1 items-center gap-y-6 md:grid-cols-3">
-            {features.map((feature, index) => (
-              <div
-                key={index}
-                className={clsx(
-                  'bg-white p-6 rounded-lg shadow-md',
-                  index === 1 && 'md:mx-auto', // Center the middle card on medium screens
-                )}
-              >
-                <Image src={feature.image} alt={feature.title} width={64} height={64} />
-                <h3 className="text-xl font-semibold mt-4">{feature.title}</h3>
-                <p className="mt-2">{feature.description}</p>
-              </div>
-            ))}
-          </div>
+          <Tab.Group
+            as="div"
+            className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
+            vertical={tabOrientation === 'vertical'}
+          >
+            {({ selectedIndex }) => (
+              <>
+                <div className="-mx-4 flex overflow-hidden pb-4 sm:mx-0 sm:overflow-hidden sm:pb-0 lg:col-span-5">
+                  <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
+                    {features.map((feature, featureIndex) => (
+                      <div
+                        key={feature.title}
+                        className={clsx(
+                          'group relative rounded-full py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6',
+                          selectedIndex === featureIndex
+                            ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
+                            : 'hover:bg-white/10 lg:hover:bg-white/5'
+                        )}
+                      >
+                        <h3>
+                          <Tab
+                            className={clsx(
+                              'font-display text-lg focus:outline-none',
+                              selectedIndex === featureIndex
+                                ? 'text-[#338161] lg:text-white'
+                                : 'text-white hover:text-white lg:text-white'
+                            )}
+                          >
+                            <span className="absolute inset-0 rounded-full lg:rounded-r-none lg:rounded-l-xl" />
+                            {feature.title}
+                          </Tab>
+                        </h3>
+                        <p
+                          className={clsx(
+                            'mt-2 hidden text-sm lg:block',
+                            selectedIndex === featureIndex
+                              ? 'text-white'
+                              : 'text-white group-hover:text-white'
+                          )}
+                        >
+                          {feature.description}
+                        </p>
+                      </div>
+                    ))}
+                  </Tab.List>
+                </div>
+                <Tab.Panels className="lg:col-span-7">
+                  {features.map((feature) => (
+                    <Tab.Panel key={feature.title} unmount={false}>
+                      <div className="relative sm:px-6 lg:hidden">
+                        <div className="absolute -inset-x-4 top-[-6.5rem] bottom-[-4.25rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
+                        <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                          {feature.description}
+                        </p>
+                      </div>
+                      <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                        <Image
+                          className="w-full"
+                          src={feature.image}
+                          alt=""
+                          priority
+                          sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+                        />
+                      </div>
+                    </Tab.Panel>
+                  ))}
+                </Tab.Panels>
+              </>
+            )}
+          </Tab.Group>
         )}
       </Container>
     </section>
