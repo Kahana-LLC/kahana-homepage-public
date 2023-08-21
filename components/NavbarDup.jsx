de898c0215f72b575f999df05ca90795e2570de5
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 21:10:36 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -6,15 +6,10 @@ import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 import HeaderBanner from './HeaderBanner';
 
 const navigation = [
-  { name: 'Pricing', href: '/pricing' },
+  { name: 'Pricing', href: 'pricing' },
   { name: 'Blog', href: 'https://blog.kahana.co' },
-  { name: 'Resources', href: '/resources' },
-];
-
-const solutionsDropdown = [
-  { name: 'Enterprise', href: '/enterprise' },
-  { name: 'Coaches & Consultants', href: '/coaches-and-consultants' },
-  { name: 'Experts', href: '/experts' },
+  { name: 'Resources', href: 'resources' },
+  { name: 'Solutions', href: 'solutions' },
 ];
 
 const navigation1 = [
@@ -22,21 +17,21 @@ const navigation1 = [
   { name: 'Log in', href: 'https://app.kahana.co/login' },
 ];
 
+const navigationAll = [
+  ...navigation,
+  ...navigation1,
+];
+
 export default function NavbarDup() {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isSolutionsDropdownOpen, setIsSolutionsDropdownOpen] = useState(false);
 
   const toggleNavOpen = () => {
     setIsNavOpen((prevState) => !prevState);
   };
 
-  const toggleSolutionsDropdown = () => {
-    setIsSolutionsDropdownOpen((prevState) => !prevState);
-  };
-
   useEffect(() => {
     const bodyScrollLock = () => {
-      if (isNavOpen || isSolutionsDropdownOpen) {
+      if (isNavOpen) {
         document.body.style.overflow = 'hidden';
       } else {
         document.body.style.overflow = 'auto';
@@ -48,7 +43,7 @@ export default function NavbarDup() {
     return () => {
       document.body.style.overflow = 'auto';
     };
-  }, [isNavOpen, isSolutionsDropdownOpen]);
+  }, [isNavOpen]);
 
   return (
     <>
@@ -66,11 +61,6 @@ export default function NavbarDup() {
             </Link>
 
             <section className="MOBILE-MENU relative flex lg:hidden">
-              <div className="HAMBURGER-ICON space-y-2" onClick={toggleNavOpen}>
-                <span className={`block h-0.5 w-8 bg-gray-600 ${isNavOpen ? 'rotate-45 translate-y-1.5' : ''}`}></span>
-                <span className={`block h-0.5 w-8 bg-gray-600 ${isNavOpen ? 'opacity-0' : ''}`}></span>
-                <span className={`block h-0.5 w-8 bg-gray-600 ${isNavOpen ? '-rotate-45 -translate-y-1.5' : ''}`}></span>
-              </div>
               <div className="MOBILE-MENU-OVERLAY fixed top-0 left-0 w-full h-screen bg-white z-10" style={{ display: isNavOpen ? 'flex' : 'none' }}>
                 <div className="CROSS-ICON absolute top-4 right-4 px-2 py-2" onClick={toggleNavOpen}>
                   <svg
@@ -88,71 +78,32 @@ export default function NavbarDup() {
                 </div>
                 <div className="MOBILE-MENU-CONTENT flex flex-col items-start justify-start h-full p-8">
                   <ul className="MENU-LINK-MOBILE-OPEN space-y-4 text-lg">
-                    {navigation1.map((link) => (
+                    {navigationAll.map((link) => (
                       <li key={link.name}>
                         <Link href={link.href} className="text-gray-600 hover:text-gray-800">
                           {link.name}
                         </Link>
                       </li>
                     ))}
-                    {navigation.map((link) => (
-                      <li key={link.name}>
-                        <Link href={link.href} className="text-gray-600 hover:text-gray-800">
-                          {link.name}
-                        </Link>
-                      </li>
-                    ))}
-                    <li key="Solutions" className="relative group">
-                      <span className="text-gray-600 hover:text-gray-800 cursor-pointer" onMouseEnter={toggleSolutionsDropdown} onMouseLeave={toggleSolutionsDropdown}>
-                        Solutions
-                      </span>
-                      <ul className={`absolute left-0 w-screen bg-white border border-gray-300 ${isSolutionsDropdownOpen ? 'block' : 'hidden'}`} style={{ zIndex: 999 }}>
-                        {solutionsDropdown.map((link) => (
-                          <li key={link.name}>
-                            <Link href={link.href} className="block px-4 py-3 text-gray-600 hover:text-gray-800">
-                              {link.name}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </li>
                   </ul>
                 </div>
               </div>
+
+              <div className="HAMBURGER-ICON space-y-2" onClick={toggleNavOpen} style={{ display: isNavOpen ? 'none' : 'block' }}>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              </div>
             </section>
 
-            <ul className="DESKTOP-MENU hidden space-x-8 lg:flex ml-4">
-              <li key="Solutions" className="relative group" onMouseEnter={toggleSolutionsDropdown} onMouseLeave={toggleSolutionsDropdown}>
-                <span className="text-base font-small text-gray-600 cursor-pointer">
-                  Solutions
-                </span>
-                <ul className={`absolute left-0 w-auto bg-white border border-gray-300 ${isSolutionsDropdownOpen ? 'block' : 'hidden'}`} style={{ zIndex: 999 }}>
-                  {solutionsDropdown.map((link) => (
-                    <li key={link.name}>
-                      <Link href={link.href} className="block px-4 py-3 text-gray-600 hover:text-gray-800">
-                        {link.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-              {navigation.map((link) => (
+            <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+              {navigationAll.map((link) => (
                 <li key={link.name}>
                   <Link href={link.href} className="text-base font-small text-gray-600 hover:text-gray-800">
                     {link.name}
                   </Link>
                 </li>
               ))}
-              <li key="Request a demo">
-                <Link href={navigation1[0].href} className="text-base font-small text-gray-600 hover:text-gray-800">
-                  {navigation1[0].name}
-                </Link>
-              </li>
-              <li key="Log in">
-                <Link href={navigation1[1].href} className="text-base font-small text-gray-600 hover:text-gray-800">
-                  {navigation1[1].name}
-                </Link>
-              </li>
             </ul>
           </div>
         </nav>
