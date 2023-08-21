d29cad598bc6a9b2ff3a1ae80cf080d198394cd8
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-20 21:05:45 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -103,7 +103,7 @@ export default function NavbarDup() {
                       </li>
                     ))}
                     <li key="Solutions" className="relative group">
-                      <span className="text-gray-600 hover:text-gray-800 cursor-pointer" onClick={toggleSolutionsDropdown}>
+                      <span className="text-gray-600 hover:text-gray-800 cursor-pointer" onMouseEnter={toggleSolutionsDropdown} onMouseLeave={toggleSolutionsDropdown}>
                         Solutions
                       </span>
                       <ul className={`absolute left-0 w-screen bg-white border border-gray-300 ${isSolutionsDropdownOpen ? 'block' : 'hidden'}`} style={{ zIndex: 999 }}>
@@ -122,18 +122,11 @@ export default function NavbarDup() {
             </section>
 
             <ul className="DESKTOP-MENU hidden space-x-8 lg:flex ml-4">
-              <li
-                className="relative group"
-                onMouseEnter={openSolutionsDropdown}
-                onMouseLeave={closeSolutionsDropdown}
-              >
+              <li key="Solutions" className="relative group" onMouseEnter={toggleSolutionsDropdown} onMouseLeave={toggleSolutionsDropdown}>
                 <span className="text-base font-small text-gray-600 cursor-pointer">
                   Solutions
                 </span>
-                <ul
-                  className={`absolute left-0 w-auto bg-white border border-gray-300 ${isSolutionsDropdownOpen ? 'block' : 'hidden'}`}
-                  style={{ zIndex: 999 }}
-                >
+                <ul className={`absolute left-0 w-auto bg-white border border-gray-300 ${isSolutionsDropdownOpen ? 'block' : 'hidden'}`} style={{ zIndex: 999 }}>
                   {solutionsDropdown.map((link) => (
                     <li key={link.name}>
                       <Link href={link.href} className="block px-4 py-3 text-gray-600 hover:text-gray-800">
