52d9af3201eec2a965c8dd909a96566b4dfdce0f
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-21 13:38:51 -0500

    Update collaboration-tools.jsx

--- pages/product/collaboration-tools.jsx
+++ pages/product/collaboration-tools.jsx
@@ -60,7 +60,7 @@ const CollaborationTools = () => {
   return (
     <div>
       <Head>
-        <title>Turn your knowledge into recurring revenue</title>
+        <title>Collaborate with others and monetize together</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
