c05afe2d491b762bffce27fe5cfdc991378ff018
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-21 23:33:38 -0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -131,8 +131,6 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
-            </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <h3 className="text-base font-medium text-gray-900">Social</h3>
                 <ul role="list" className="mt-4 space-y-4">
@@ -148,6 +146,8 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <h3 className="text-base font-medium text-gray-900">Build</h3>
                 <ul role="list" className="mt-4 space-y-4">
