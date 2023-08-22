72069325062d1642d91820b67a8867b21410b9a5
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 17:18:16 -0500

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -104,7 +104,7 @@ export default function HeroSection() {
                           <a
                             href="https://app.kahana.co/signup"
                             className="flex-1 rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
-                            style={{ maxWidth: "40%" }}
+                            style={{ maxWidth: "40%", marginLeft: "20px" }}
                           >
                             Sign up for free
                           </a>
