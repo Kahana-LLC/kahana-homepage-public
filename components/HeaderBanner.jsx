c30df4d24758ebfab90550b923a4a871af63560b
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 18:29:00 -0500

    Update HeaderBanner.jsx

--- components/HeaderBanner.jsx
+++ components/HeaderBanner.jsx
@@ -2,7 +2,7 @@ export default function HeaderBanner() {
   return (
     <div className="relative bg-gradient-to-l from-green-200 via-[#038270] to-[#338161]">
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
-        <div className="pr-16 sm:px-16 text-center">
+        <div className="pr-16 sm:px-16 sm:text-center">
           <p className="text-base text-white">
             {/* <span className="md:hidden">We announced a new product!</span> */}
             {/* <span>Check out today&apos;s</span> */}
