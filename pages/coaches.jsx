4d89eb5c38b234df5e41bfdf6334ccff3f3e1d0d
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-21 23:55:50 -0500

    Update and rename coaches-and-consultants.jsx to coaches.jsx

--- pages/coaches.jsx
+++ pages/coaches.jsx
@@ -11,7 +11,7 @@ const CoachesPage = () => {
   return (
     <div>
       <Head>
-        <title>Kahana for coaches & consultants</title>
+        <title>Kahana for coaches</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
