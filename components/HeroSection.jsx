a062c8d55b3e46b2bd405628beb8634ee078a92d
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 17:24:23 -0500

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -100,7 +100,7 @@ export default function HeroSection() {
                           />
                         </div>
 
-                        <div className="flex justify-center items-center mx-auto">
+                        <div className="flex justify-center items-center space-x-3 mx-auto">
                           <a
                             href="https://app.kahana.co/signup"
                             className="flex-1 rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
