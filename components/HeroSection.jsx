4dd3d6b5ac971f93d082326a82210a5899249db8
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 15:30:20 -0500

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -107,13 +107,21 @@ export default function HeroSection() {
                           />
                         </div>
 
-                        <div>
+                        <div className="flex justify-center items-center space-x-6">
                           <a
                             href="https://app.kahana.co/signup"
-                            className="flex w-full justify-center rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 mb-3"
+                            className="flex-1 rounded-md border border-transparent bg-[#038270] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2"
                           >
-                              Sign up for free
+                            Sign up for free
                           </a>
+                          <span className="flex-1 text-sm font-medium text-gray-700">
+                            <a
+                              href="https://7hkdcfzbmr0.typeform.com/to/ZYLHazEf?utm_content=landing_page_CTA"
+                              className="hover:underline"
+                            >
+                              Request a demo
+                            </a>
+                          </span>
                         </div>
                     </div>
                   </div>
