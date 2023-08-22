15fc3b1af741908330c062e0af41d3c2943497b0
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 18:13:37 -0500

    Update Faq.jsx

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -88,7 +88,7 @@ export default function Faq() {
       <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
         <div className="lg:grid lg:grid-cols-3 lg:gap-8">
           <div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 text-center sm:text-left">
               Frequently asked questions
             </h2>
             <p className="mt-4 text-lg text-gray-500">
