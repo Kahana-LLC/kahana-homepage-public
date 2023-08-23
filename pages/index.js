3f50b8bf6a8e675a6661f60d9ca2adca3553c0e8
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-22 20:02:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -86,7 +86,7 @@ export default function Home() {
           
           <section className={`py-16 px-4 sm:px-6 lg:px-8 bg-white`}>
             <div className="max-w-7xl mx-auto text-center">
-              <h2 className={`text-3xl font-bold ${showImage ? 'mb-2' : 'mb-4'} text-gray-900`}>
+              <h2 className={`text-3xl font-bold text-gray-900`}>
                 Become an affiliate
               </h2>
               <p className="text-gray-700">
