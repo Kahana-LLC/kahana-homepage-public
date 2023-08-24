07aa20fc09279c756f26a4fe4b7a841f5bff2988
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-23 22:07:05 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -13,12 +13,6 @@ const Navigation = () => {
 
   return (
     <nav className={`navigation ${isOpen ? 'open' : ''}`}>
-      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-
       <div className="logo">
         <Link href="/">
           <a>
@@ -31,6 +25,12 @@ const Navigation = () => {
             />
           </a>
         </Link>
+      </div>
+
+      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
 
       <div className="menu">
@@ -99,43 +99,140 @@ const Navigation = () => {
       </div>
 
       <style jsx>{`
+        .navigation {
+          background-color: white;
+          color: black;
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          padding: 0.5rem 2rem;
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          transition: background-color 0.3s ease-in-out;
+        }
+
+        .open .navigation {
+          background-color: white;
+          color: black;
+        }
+
+        .logo {
+          cursor: pointer;
+          flex: 1;
+        }
+
+        .menu {
+          display: flex;
+          align-items: center;
+          margin-left: auto;
+        }
+
+        .menu ul {
+          list-style: none;
+          display: flex;
+          margin: 0;
+          padding: 0;
+        }
+
+        .menu-item {
+          text-decoration: none;
+          color: black;
+          padding: 0.5rem 1rem;
+          transition: background-color 0.3s ease-in-out;
+          cursor: pointer;
+        }
+
+        .menu-item:hover {
+          background-color: #024324;
+        }
+
+        .dropdown {
+          position: relative;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #038270;
+          min-width: 160px;
+          z-index: 1;
+          cursor: default;
+        }
+
+        .dropdown:hover .dropdown-content {
+          display: block;
+        }
+
+        .dropdown-content a {
+          color: white;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+        }
+
+        .login {
+          background-color: #038270;
+        }
+
+        .login:hover {
+          background-color: #024324;
+        }
+
+        .hamburger {
+          display: none;
+          flex-direction: column;
+          cursor: pointer;
+        }
+
+        .hamburger.open .bar:nth-child(1) {
+          transform: rotate(-45deg) translate(-5px, 6px);
+        }
+
+        .hamburger.open .bar:nth-child(2) {
+          opacity: 0;
+        }
+
+        .hamburger.open .bar:nth-child(3) {
+          transform: rotate(45deg) translate(-5px, -6px);
+        }
+
         @media (max-width: 768px) {
           .navigation {
-            padding: 0.5rem 2rem;
-          }
-
-          .logo {
-            text-align: left;
-            padding-right: 1rem;
+            padding: 0.5rem 2rem; /* Adjust padding as needed */
           }
 
           .menu {
             display: none;
             flex-direction: column;
             position: absolute;
-            top: 60px;
+            top: 60px; /* Adjust top position as needed */
             right: 0;
             width: 100%;
-            background-color: white;
+            background-color: white; /* Change background color for mobile */
+            z-index: 2; /* Ensure that the menu is above other elements */
           }
 
           .open .menu {
             display: flex;
           }
 
+          .menu ul {
+            flex-direction: column;
+          }
+
           .menu-item {
-            padding: 1rem 2rem;
-            color: black;
+            padding: 1rem 2rem; /* Adjust padding as needed */
+            color: black; /* Change text color for mobile */
           }
 
           .hamburger {
             display: flex;
-            flex-direction: column;
-            cursor: pointer;
             position: absolute;
-            top: 0.5rem;
-            right: 1rem;
-            z-index: 3;
+            top: 0.5rem; /* Adjust the positioning as needed */
+            right: 1rem; /* Adjust the positioning as needed */
+            z-index: 3; /* Ensure that the hamburger menu is above everything */
           }
         }
       `}</style>
@@ -144,3 +241,4 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
