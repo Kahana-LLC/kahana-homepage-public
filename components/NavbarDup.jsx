5338b04d50705a6465eca0bb229e3c9ff5c8ca1a
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-24 15:10:28 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -6,35 +6,26 @@ import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [showSolutionsDropdown, setShowSolutionsDropdown] = useState(false);
-  const [showResourcesDropdown, setShowResourcesDropdown] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  const toggleSolutionsDropdown = () => {
-    setShowSolutionsDropdown(!showSolutionsDropdown);
-    setShowResourcesDropdown(false);
-  };
-
-  const toggleResourcesDropdown = () => {
-    setShowResourcesDropdown(!showResourcesDropdown);
-    setShowSolutionsDropdown(false);
-  };
-
   return (
     <nav className={`navigation ${isOpen ? 'open' : ''}`}>
-      <div className="mobile-header">
-        <Link href="/" aria-label="Home">
-          <Image className="h-10" src={whiteKahanaLogo} alt="navbar-logo" />
-        </Link>
+      <Link href="/" aria-label="Home">
+        <span className="sr-only">Company</span>
+        <Image
+          className="h-10"
+          src={whiteKahanaLogo}
+          alt="navbar-logo"
+        />
+      </Link>
 
-        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-        </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
       </div>
 
       <div className={`menu ${isOpen ? 'open' : ''}`}>
@@ -49,10 +40,8 @@ const Navigation = () => {
               <a className="menu-item">Examples</a>
             </Link>
           </li>
-          <li className={`dropdown ${showSolutionsDropdown ? 'open' : ''}`}>
-            <span className="menu-item" onClick={toggleSolutionsDropdown}>
-              Solutions
-            </span>
+          <li className="dropdown">
+            <span className="menu-item">Solutions</span>
             <div className="dropdown-content">
               <Link href="/enterprise">
                 <a>For Enterprise</a>
@@ -71,10 +60,8 @@ const Navigation = () => {
               </Link>
             </div>
           </li>
-          <li className={`dropdown ${showResourcesDropdown ? 'open' : ''}`}>
-            <span className="menu-item" onClick={toggleResourcesDropdown}>
-              Resources
-            </span>
+          <li className="dropdown">
+            <span className="menu-item">Resources</span>
             <div className="dropdown-content">
               <a href="https://blog.kahana.co/">Blog</a>
               <Link href="/resources">
@@ -107,41 +94,112 @@ const Navigation = () => {
       </div>
 
       <style jsx>{`
-        /* Existing styles for desktop version here */
-
-        /* Additional styles for mobile version */
-        .mobile-header {
+        .navigation {
+          background-color: white;
+          color: black;
           display: flex;
-          justify-content: space-between;
+          justify-content: flex-end;
           align-items: center;
           padding: 0.5rem 2rem;
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          border-bottom: 1px solid lightgray; /* Add a light gray border */
+        }
+
+        .open .navigation {
           background-color: white;
-          border-bottom: 1px solid lightgray;
+          color: black;
+        }
+
+        .logo {
+          cursor: pointer;
+          flex: 1;
         }
 
         .menu {
-          margin-top: 70px; /* Add space below the navigation bar */
+          display: flex;
+          align-items: center;
+          margin-left: auto;
         }
 
-        /* Media query for mobile styles */
-        @media (max-width: 768px) {
-          /* Hide desktop navigation */
-          .navigation {
-            display: none;
-          }
+        .menu ul {
+          list-style: none;
+          display: flex;
+          margin: 0;
+          padding: 0;
+        }
 
-          /* Show mobile navigation */
-          .mobile-header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
+        .menu-item {
+          text-decoration: none;
+          color: black;
+          padding: 0.5rem 1rem;
+          transition: background-color 0.3s ease-in-out;
+          cursor: pointer;
+        }
+
+        .menu-item:hover {
+          background-color: #024324;
+        }
+
+        .dropdown {
+          position: relative;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #038270;
+          min-width: 160px;
+          z-index: 1;
+          cursor: default;
+        }
+
+        .dropdown:hover .dropdown-content {
+          display: block;
+        }
+
+        .dropdown-content a {
+          color: white;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+        }
+
+        .login {
+          background-color: #038270;
+        }
+
+        .login:hover {
+          background-color: #024324;
+        }
+
+        .hamburger {
+          display: none;
+          flex-direction: column;
+          cursor: pointer;
+        }
+
+        .hamburger.open .bar:nth-child(1) {
+          transform: rotate(-45deg) translate(-5px, 6px);
+        }
+
+        .hamburger.open .bar:nth-child(2) {
+          opacity: 0;
+        }
+
+        .hamburger.open .bar:nth-child(3) {
+          transform: rotate(45deg) translate(-5px, -6px);
+        }
+
+        @media (max-width: 768px) {
+          .navigation {
             padding: 0.5rem 2rem;
-            background-color: white;
-            border-bottom: 1px solid lightgray;
           }
 
           .menu {
-            display: flex;
+            display: none;
             flex-direction: column;
             position: absolute;
             top: 60px;
