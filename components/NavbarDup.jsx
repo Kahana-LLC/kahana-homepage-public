69901101c1c024c91a0fd7c4819a3c62c3a26004
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-25 17:38:09 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -255,7 +255,7 @@ function NavBar() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M3 12h18"
+                    d="M4 6h16M4 12h16M4 18h16"
                   ></path>
                 </svg>
               )}
