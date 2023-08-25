0c5880a3f55cf18de066e672794669fabfb523e6
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-24 20:34:58 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import whiteKahanaLogo from '../assets/kahana_logo_wide_light.svg';
-
-const NavMenu = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+function NavBar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
-    setMobileMenuOpen(!mobileMenuOpen);
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
     <nav className="bg-white border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex-shrink-0 flex items-center">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center py-4">
+          {/* Logo */}
+          <div className="flex items-center">
             <Link href="/" aria-label="Home">
               <span className="sr-only">Company</span>
               <Image
@@ -25,130 +24,245 @@ const NavMenu = () => {
               />
             </Link>
           </div>
+
+          {/* Desktop Menu */}
           <div className="hidden md:flex space-x-4">
-            <NavLink href="/about">About</NavLink>
-            <NavLink href="/explore">Examples</NavLink>
-            <Dropdown title="Solutions">
-              <DropdownItem href="/enterprise">For Enterprise</DropdownItem>
-              <DropdownItem href="/coaches">For Coaches</DropdownItem>
-              <DropdownItem href="/consultants">For Consultants</DropdownItem>
-              <DropdownItem href="/experts">For Experts</DropdownItem>
-              <DropdownItem href="/affiliates">Become an affiliate</DropdownItem>
-            </Dropdown>
-            <Dropdown title="Resources">
-              <DropdownItem href="https://blog.kahana.co/">Blog</DropdownItem>
-              <DropdownItem href="/resources">Monetizing Notion</DropdownItem>
-              <DropdownItem href="/resources">Monetizing Google Drive</DropdownItem>
-              <DropdownItem href="/resources">Selling Digital Products</DropdownItem>
-              <DropdownItem href="https://kahana.tawk.help/">Help Center</DropdownItem>
-              <DropdownItem href="/faq">FAQ</DropdownItem>
-              <DropdownItem href="https://nas.io/creators-and-experts">Community</DropdownItem>
-            </Dropdown>
-            <NavLink href="/pricing">Pricing</NavLink>
-            <LoginButton />
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+            <Link href="/explore" className="nav-link">
+              Examples
+            </Link>
+            <div className="dropdown">
+              <button className="nav-link">Solutions</button>
+              <ul className="absolute hidden mt-2 space-y-2 bg-white border border-gray-200 w-48">
+                <li>
+                  <Link href="/enterprise" className="dropdown-link">
+                    For Enterprise
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/coaches" className="dropdown-link">
+                    For Coaches
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/consultants" className="dropdown-link">
+                    For Consultants
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/experts" className="dropdown-link">
+                    For Experts
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/affiliates" className="dropdown-link">
+                    Become an affiliate
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown">
+              <button className="nav-link">Resources</button>
+              <ul className="absolute hidden mt-2 space-y-2 bg-white border border-gray-200 w-48">
+                <li>
+                  <a
+                    href="https://blog.kahana.co/"
+                    className="dropdown-link"
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Monetizing Notion
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Monetizing Google Drive
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Selling Digital Products
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    href="https://kahana.tawk.help/"
+                    className="dropdown-link"
+                  >
+                    Help Center
+                  </a>
+                </li>
+                <li>
+                  <Link href="/faq" className="dropdown-link">
+                    FAQ
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    href="https://nas.io/creators-and-experts"
+                    className="dropdown-link"
+                  >
+                    Community
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <Link href="/pricing" className="nav-link">
+              Pricing
+            </Link>
           </div>
-          {/* Hamburger Menu */}
+
+          {/* Mobile Menu */}
           <div className="md:hidden flex items-center">
             <button
               onClick={toggleMobileMenu}
-              className="text-gray-600 hover:text-gray-900 focus:outline-none"
+              className="hamburger-button"
+              aria-label="Mobile Menu"
             >
-              <svg
-                className={`h-6 w-6 ${mobileMenuOpen ? 'hidden' : 'block'}`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className={`h-6 w-6 ${mobileMenuOpen ? 'block' : 'hidden'}`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              {isMobileMenuOpen ? (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  ></path>
+                </svg>
+              )}
             </button>
           </div>
         </div>
       </div>
-      {mobileMenuOpen && (
-        <div className="md:hidden">
-          <MobileMenuItem href="/about">About</MobileMenuItem>
-          <MobileMenuItem href="/explore">Examples</MobileMenuItem>
-          <Dropdown title="Solutions">
-            <DropdownItem href="/enterprise">For Enterprise</DropdownItem>
-            <DropdownItem href="/coaches">For Coaches</DropdownItem>
-            <DropdownItem href="/consultants">For Consultants</DropdownItem>
-            <DropdownItem href="/experts">For Experts</DropdownItem>
-            <DropdownItem href="/affiliates">Become an affiliate</DropdownItem>
-          </Dropdown>
-          <Dropdown title="Resources">
-            <DropdownItem href="https://blog.kahana.co/">Blog</DropdownItem>
-            <DropdownItem href="/resources">Monetizing Notion</DropdownItem>
-            <DropdownItem href="/resources">Monetizing Google Drive</DropdownItem>
-            <DropdownItem href="/resources">Selling Digital Products</DropdownItem>
-            <DropdownItem href="https://kahana.tawk.help/">Help Center</DropdownItem>
-            <DropdownItem href="/faq">FAQ</DropdownItem>
-            <DropdownItem href="https://nas.io/creators-and-experts">Community</DropdownItem>
-          </Dropdown>
-          <MobileMenuItem href="/pricing">Pricing</MobileMenuItem>
-          <LoginButton />
+
+      {/* Mobile Menu Content */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden bg-white py-4">
+          <div className="flex flex-col items-center space-y-4">
+            <Link href="/about" className="mobile-link">
+              About
+            </Link>
+            <Link href="/explore" className="mobile-link">
+              Examples
+            </Link>
+            <div className="dropdown">
+              <button className="mobile-link">Solutions</button>
+              <ul className="absolute hidden space-y-2 bg-white border border-gray-200 w-48">
+                <li>
+                  <Link href="/enterprise" className="dropdown-link">
+                    For Enterprise
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/coaches" className="dropdown-link">
+                    For Coaches
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/consultants" className="dropdown-link">
+                    For Consultants
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/experts" className="dropdown-link">
+                    For Experts
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/affiliates" className="dropdown-link">
+                    Become an affiliate
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown">
+              <button className="mobile-link">Resources</button>
+              <ul className="absolute hidden space-y-2 bg-white border border-gray-200 w-48">
+                <li>
+                  <a
+                    href="https://blog.kahana.co/"
+                    className="dropdown-link"
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Monetizing Notion
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Monetizing Google Drive
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/resources" className="dropdown-link">
+                    Selling Digital Products
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    href="https://kahana.tawk.help/"
+                    className="dropdown-link"
+                  >
+                    Help Center
+                  </a>
+                </li>
+                <li>
+                  <Link href="/faq" className="dropdown-link">
+                    FAQ
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    href="https://nas.io/creators-and-experts"
+                    className="dropdown-link"
+                  >
+                    Community
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <Link href="/pricing" className="mobile-link">
+              Pricing
+            </Link>
+            <a
+              href="https://app.kahana.co/login"
+              className="mobile-link bg-green-500 rounded-md py-2 px-4"
+            >
+              Log in
+            </a>
+          </div>
         </div>
       )}
     </nav>
   );
-};
+}
 
-const NavLink = ({ href, children }) => (
-  <Link href={href}>
-    <a className="text-gray-600 hover:text-gray-900">{children}</a>
-  </Link>
-);
-
-const Dropdown = ({ title, children }) => (
-  <div className="dropdown">
-    <button className="text-gray-600 hover:text-gray-900">{title}</button>
-    <div className="hidden absolute z-10 left-0 mt-2 space-y-2 bg-white border border-gray-200 group-hover:block">
-      {children}
-    </div>
-  </div>
-);
-
-const DropdownItem = ({ href, children }) => (
-  <Link href={href}>
-    <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-      {children}
-    </a>
-  </Link>
-);
-
-const LoginButton = () => (
-  <a
-    href="https://app.kahana.co/login"
-    className="bg-green-600 hover:bg-green-800 text-white px-4 py-2 rounded-full transition-colors duration-300"
-  >
-    Log in
-  </a>
-);
-
-const MobileMenuItem = ({ href, children }) => (
-  <Link href={href}>
-    <a className="block py-2 px-4 text-gray-700 hover:bg-gray-100">{children}</a>
-  </Link>
-);
-
-export default NavMenu;
+export default NavBar;
