9c4cd5fe0372bb4eb15079ae94fbc924692c7e23
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-25 17:32:04 -0500

    Update NavbarDup.jsx

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -436,10 +436,10 @@ function NavBar() {
           </div>
           <hr className="w-full border-gray-200 mt-2 mb-2" />
           <div className="flex flex-col items-center mt-4">
-            <Link href="https://app.kahana.co/signup" className="mobile-link bg-[#038270] hover:bg-[#024324] rounded-md h-7 w-48 text-white text-center flex justify-center items-center">
+            <Link href="https://app.kahana.co/signup" className="mobile-link bg-[#038270] hover:bg-[#024324] rounded-md h-10 w-60 text-white text-center flex justify-center items-center">
               Get Kahana free
             </Link>
-            <Link href="https://app.kahana.co/login" className="mobile-link bg-[#FFFFFF] hover:bg-[#f2f2f2] border-gray-300 border rounded-md h-7 w-48 mt-2 text-center flex justify-center items-center">
+            <Link href="https://app.kahana.co/login" className="mobile-link bg-[#FFFFFF] hover:bg-[#f2f2f2] border-gray-300 border rounded-md h-10 w-60 mt-2 text-center flex justify-center items-center">
               Log in
             </Link>
           </div>
