e31bdfe02c285634d323c2cdc14de92774a71ce3
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-27 14:54:30 -0500

    Update order-hubs-on-demand.jsx

--- pages/order-hubs-on-demand.jsx
+++ pages/order-hubs-on-demand.jsx
@@ -54,7 +54,7 @@ export default function AffiliateProgramPage() {
   return (
     <>
       <Head>
-        <title>Kahana Affiliate Program</title>
+        <title>Hubs On-Demand</title>
         <meta
           name="Kahana"
           content="Kahana is the easiest way to monetize your content and research. Transform knowledge and expertise into subscription revenue. Sign up for free today!"
@@ -87,6 +87,7 @@ export default function AffiliateProgramPage() {
               src={OnDemandHub}
               alt="Example of a Created Hub"
               layout="responsive"
+              height={800}
               objectFit="cover"
               className="hidden md:block"
             />
