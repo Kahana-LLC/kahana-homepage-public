f342bff7e1bcdb3a320ed5b7ace23172ad615859
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-08-30 18:46:54 -0500

    Update order-hubs-on-demand.jsx

--- pages/order-hubs-on-demand.jsx
+++ pages/order-hubs-on-demand.jsx
@@ -130,6 +130,26 @@ export default function AffiliateProgramPage() {
             `,
           }}
         />
+        {/* Reditus affiliate tracking script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w, d, s, p, t) {
+                w.gr = w.gr || function() {
+                  w.gr.q = w.gr.q || [];
+                  w.gr.q.push(arguments);
+                };
+                p = d.getElementsByTagName(s)[0];
+                t = d.createElement(s);
+                t.async = true;
+                t.src = "https://app.getreditus.com/gr.js?_ce=90";
+                p.parentNode.insertBefore(t, p);
+              })(window, document, "script");
+              gr("track", "pageview");
+            `,
+          }}
+        />
+        {/* Stripe button script */}
         <script async
           src="https://js.stripe.com/v3/buy-button.js">
         </script>
