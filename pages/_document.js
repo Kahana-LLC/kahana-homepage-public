c73984963fd8e004b0923e25e87450ba95301e04
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-09-26 16:28:39 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WBXNXKQ');
+            `,
+          }}
+        />
+  
         {/* Adding hotjar script here */}
-
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -38,6 +50,16 @@ export default function Document() {
 
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) immediately after opening <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBXNXKQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <Main />
         <NextScript />
       </body>
