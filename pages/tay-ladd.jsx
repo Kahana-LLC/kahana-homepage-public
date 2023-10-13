4987ea189c19380d04782196861d5a8155282b18
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-10-13 07:48:41 -0500

    Update tay-ladd.jsx

--- pages/tay-ladd.jsx
+++ pages/tay-ladd.jsx
@@ -186,12 +186,6 @@ export default function TayLaddPage() {
                     >
                       <span className="sr-only">LinkedIn</span>
                       <svg
-                        className="h-8 w-8"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                      <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
