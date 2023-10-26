47a6e1ec28b24045d37aa3e74f3765c752b7a433
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-10-26 01:49:28 -0500

    Update CustomerSuccessSection.jsx

--- components/CustomerSuccessSection.jsx
+++ components/CustomerSuccessSection.jsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import wAmy from '../assets/images/wAmySlider.png';
-import Kelsey from '../assets/images/kelseySlider.png';
-import Juice from '../assets/images/juiceSlider.png';
-import Olivia from '../assets/images/oliviaSlider.png';
+import KelseySuccess from '../assets/images/kelseySuccess.png';
 
 //icons
 import { CheckIcon } from './CheckIcon';
@@ -48,90 +45,29 @@ export default function HeroSection() {
     setShowVideo(false);
   };
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [wAmy, Kelsey, Juice, Olivia];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      nextSlide();
-    }, 5000); // Change the interval duration (in milliseconds) as needed
-    return () => clearInterval(interval);
-  }, [currentIndex]);
-
-  const nextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-  };
-
   return (
     <div className=" bg-white">
       <div className="pt-1 pb-16 sm:pb-24">
         <main className="mt-10 sm:mt-24">
           <div className="mx-auto max-w-7xl">
-            <div className="lg:grid lg:grid-cols-12 lg:gap-8 lg:items-center">
-              <div className="lg:col-span-7 px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:flex lg:items-center lg:text-left">
+            <div className="lg:gap-8 lg:items-center">
+              <div className="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:flex lg:items-center lg:text-left">
                 <div>
-
-                  {/* animated background left blob*/}
-
-                  <div className="invisible md:visible absolute top-15 -left-7 w-40 h-40 bg-[#038270] rounded-full filter blur-3xl opacity-50 animate-blob"></div>
+                  
                   <div>
 
-                    <h1 className="py-4  bg-clip-text text-transparent bg-gradient-to-r from-[#024324] to-teal-300 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl text-center sm:text-left">
-                      Add subscription revenue to your portfolio for free
+                    <h1 className="py-4  bg-clip-text text-transparent bg-black text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl text-center sm:text-left">
+                      How Kelsey turned her Pinterest knowledge into a 4-figure income stream 
                     </h1>
 
-                    <p className="mt-3 text-base text-slate-900 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl text-center sm:text-left">
-                      Make a living doing what you love. Share your best insights with your audience so they can thrive.
-                    </p>
-
-                    <div className="mt-8 flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-                      <a
-                        href="https://app.kahana.co/signup"
-                        className="rounded-md border border-transparent bg-[#3B675E] py-3 px-6 text-lg font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
-                      >
-                        Start earning 
-                      </a>
-                      <a
-                        href="#"
-                        id="open-video"
-                        className="rounded-md border border-black bg-transparent py-3 px-6 text-lg font-medium text-black shadow-sm hover:bg-[#024324] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
-                        onClick={openVideo}
-                      >
-                        Watch demo
-                      </a>
-                    </div>
-
-                    {showVideo && (
-                      <div id="video-container" className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-50">
-                        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                          <button id="close-video" className="text-2xl text-white absolute top-0 right-0 p-2 cursor-pointer" onClick={closeVideo}>✕</button>
-                          <div className="video-wrapper" style={{ maxWidth: "90%", maxHeight: "80vh" }}>
-                            <iframe
-                              id="demo-video"
-                              width={window.innerWidth >= 768 ? 560 : 350}
-                              height="315"
-                              src="https://www.youtube.com/embed/TBNzzUQsB-4"
-                              title="Demo Video"
-                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                              frameBorder="0"
-                              allowFullScreen
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                    
-                    {/* <div className="grid grid-cols-2 gap-4 mt-6 tracking-tight hidden md:block">
+                    <div className="gap-4 mt-6 tracking-tight">
+                    {/* <div className="grid grid-cols-2 gap-4 mt-6 tracking-tight hidden md:block"> */}
                       {' '}
                       <ul role="list" className=" space-y-3">
                         {[
-                          'No credit card required',
-                          'Share your unique knowledge',
-                          'Start monetizing within 1 hour',
+                          'One of Kelsey\'s services is a 1:1 strategy session where she coaches clients on setting up their business profiles',
+                          'Kelsey recorded herself sharing the same advice, best practices, and templates she shares with clients in these sessions, gathered the resources she provides them, and uploaded them into a Kahana hub',
+                          'She connected her account to Stripe to accept payments, set her price point, incorporated it onto her website, and started getting sales!',
                         ].map((feature) => (
                           <>
                             {' '}
@@ -142,19 +78,26 @@ export default function HeroSection() {
                           </>
                         ))}
                       </ul>
-                    </div> */}
+                    </div>
                     
+                    <div className="mt-8 flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+                      <a
+                        href="https://app.kahana.co/signup"
+                        className="rounded-md border border-transparent bg-[#3B675E] py-3 px-6 text-lg font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
+                      >
+                        See how Kelsey did it 
+                      </a>
+                    </div>              
                   </div>
-                  {/* right animated blob */}
                 </div>
               </div>
               
-              {/* Slider IN CASE */}
-              <div className="lg:col-span-5 hidden lg:block flex flex-col justify-center items-center"> {/* Hide on medium and below screens */}
+              {/* Image */}
+              <div className="flex flex-col justify-center items-center"> {/* Hide on medium and below screens */}
                 <Image
-                  src={images[currentIndex]}
-                  alt={`Slider Image ${currentIndex}`}
-                  width={400} // Adjust the width and height as needed
+                  src={KelseySuccess}
+                  alt={`Kelsey's Success Metrics`}
+                  width={400}
                   height={450}
                   layout="responsive"
                   objectFit="cover"
