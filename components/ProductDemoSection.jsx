d03a550f64e1ea749bfb1811bb6b360f9a3f9514
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-10-26 14:56:07 -0500

    Update ProductDemoSection.jsx

--- components/ProductDemoSection.jsx
+++ components/ProductDemoSection.jsx
@@ -11,16 +11,16 @@ import { Container } from './Container';
 
 const features = [
   {
-    title: 'Share',
-    description:
-      'Flip a switch and get in front of viewers all over the world. Get exposure to folks who can’t wait for your stuff.',
-    image: explore,
-  },
-  {
     title: 'Monetize',
     description:
       'Create a delightfully casual subscription flow by charging access to your hubs. Connect through Stripe to receive payments.',
     image: monetize,
+  },
+  {
+    title: 'Share',
+    description:
+      'Flip a switch and get in front of viewers all over the world. Get exposure to folks who can’t wait for your stuff.',
+    image: explore,
   },
   {
     title: 'Collaborate',
