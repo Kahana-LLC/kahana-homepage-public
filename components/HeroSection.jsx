22bffd23e2e6e7439d25ba73a3f20ddeacbe1284
jgans888 <56483947+jgans888@users.noreply.github.com>
2023-10-26 00:45:39 -0500

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -147,8 +147,8 @@ export default function HeroSection() {
                 <Image
                   src={images[currentIndex]}
                   alt={`Slider Image ${currentIndex}`}
-                  width={200} // Adjust the width and height as needed
-                  height={300}
+                  width={400} // Adjust the width and height as needed
+                  height={400}
                   layout="responsive"
                   objectFit="cover"
                   quality={100}
