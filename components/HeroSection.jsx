3c3ddddbbb64981d5506e31791e11615f2f2927f
jgans888 <56483947+jgans888@users.noreply.github.com>
2024-01-10 01:51:44 -0600

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -71,18 +71,18 @@ export default function HeroSection() {
                   <div className="invisible md:visible absolute top-15 -left-7 w-40 h-40 bg-[#038270] rounded-full filter blur-3xl opacity-50 animate-blob"></div>
                   <div>
 
-                    <h1 className="py-4  bg-clip-text text-transparent bg-gradient-to-r from-[#024324] to-teal-300 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl text-center sm:text-left">
+                    <h1 className="py-4  bg-clip-text text-transparent bg-gradient-to-r from-[#024324] to-teal-300 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-5xl text-center sm:text-left">
                       Build a secure knowledge business in minutes
                     </h1>
 
                     <p className="mt-3 text-base text-slate-900 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl text-center sm:text-left">
-                      Protect your digital products from getting stolen. Create new products 10x faster. Earn more income.
+                      Protect your digital products from getting stolen, create new products 10x faster, and earn more income.
                     </p>
 
                     <div className="mt-8 flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
                       <a
                         href="https://app.kahana.co/signup"
-                        className="rounded-md border border-transparent bg-[#44B6A6] py-3 px-12 text-lg font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
+                        className="rounded-md border border-transparent bg-[#44B6A6] py-3 px-7 text-lg font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
                       >
                         Earn income for free
                       </a>
