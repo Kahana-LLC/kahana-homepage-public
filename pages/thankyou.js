6deca03691e9b0a7c095fcaddb892b56142bd4f5
AdamKershner <adam@kahana.co>
2024-04-03 11:40:23 -0500

    Added thank you page

---
+++ pages/thankyou.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Link from "next/link";
+import Footer from "../components/Footer";
+import NavbarDup from "../components/NavbarDup";
+
+export default function ThankYou() {
+  return (
+    <>
+      <Head>
+        <title>Thank You!</title>
+      </Head>
+      <div>
+        <div style={{ zIndex: "100" }} className="sticky top-0">
+          <NavbarDup />
+        </div>
+        <main>
+          <section className={`py-16 px-4 sm:px-6 lg:px-8 bg-white`}>
+            <div className="max-w-7xl mx-auto text-center">
+              <h1 className={`text-3xl font-bold text-gray-900`}>Thank You!</h1>
+              <p
+                className="mt-4 text-gray-700 text-xl"
+                style={{ marginBottom: "20px" }}
+              >
+                We appreciate your support.
+              </p>
+              <div>
+                <Link href="/">
+                  <button className="px-6 py-2 bg-[#3B675E] text-white rounded-md shadow-md hover:bg-[#046856]">
+                    Back to Home
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
