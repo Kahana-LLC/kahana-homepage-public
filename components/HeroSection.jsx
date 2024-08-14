8aa1c04e039ffd498948930959cae7a62a2685e6
AdamKershner <adam@kahana.co>
2024-08-14 14:33:57 -0500

    Update text density

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -66,10 +66,10 @@ export default function HeroSection() {
                   <div className="invisible md:visible absolute top-15 -left-7 w-40 h-40 bg-[#038270] rounded-full filter blur-3xl opacity-50 animate-blob"></div>
                   <div>
                     <h1 className="py-4  bg-clip-text text-transparent bg-gradient-to-r from-[#024324] to-teal-300 text-4xl font-bold tracking-tight sm:text-5xl md:text-5xl text-center sm:text-left">
-                      All-in-one platform to monetize knowledge through digital products
+                      Monetize knowledge through digital products
                     </h1>
                     <p className="mt-3 text-base text-slate-900 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl text-center sm:text-left">
-                    Kahana is a simple yet powerful platform to curate, bundle, and package digital products into hubs. Set your own prices and earn whenever anybody pays to access your hubs. Collaborate to accelerate the process even more.
+                    Kahana is a powerful platform to create, bundle, and sell digital products in hubs. Set your prices and earn every time someone pays to access your hubs. Collaborate to speed up the process.
 
                     </p>
                     <div className="mt-8 flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
