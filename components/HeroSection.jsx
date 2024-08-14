35cac9819d87a5bee8c120c553ca2128d68fd4d5
AdamKershner <adam@kahana.co>
2024-08-14 12:13:53 -0500

    Fix page loading and improve copy

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -67,17 +67,18 @@ export default function HeroSection() {
                   <div className="invisible md:visible absolute top-15 -left-7 w-40 h-40 bg-[#038270] rounded-full filter blur-3xl opacity-50 animate-blob"></div>
                   <div>
                     <h1 className="py-4  bg-clip-text text-transparent bg-gradient-to-r from-[#024324] to-teal-300 text-4xl font-bold tracking-tight sm:text-5xl md:text-5xl text-center sm:text-left">
-                      Build a secure knowledge business in minutes
+                      All-in-one platform to monetize knowledge through digital products
                     </h1>
                     <p className="mt-3 text-base text-slate-900 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl text-center sm:text-left">
-                      Protect your digital products from getting stolen, create new products 10x faster, and earn more income.
+                    Kahana is a simple yet powerful platform to curate, bundle, and package digital products into hubs. Set your own prices and earn whenever anybody pays to access your hubs. Collaborate to accelerate the process even more.
+
                     </p>
                     <div className="mt-8 flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
                       <a
                         href="https://app.kahana.co/signup"
                         className="rounded-md border border-transparent bg-[#44B6A6] py-3 px-6 text-lg font-medium text-white shadow-sm hover:bg-[#024324] focus:outline-none focus:ring-2 focus:ring-[#024324] focus:ring-offset-2 text-center"
                       >
-                        Earn income for free
+                        Start building for free
                       </a>
                     </div>
                   </div>
