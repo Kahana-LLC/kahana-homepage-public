2468d844a66b2b9da088f31c47dd14feaf2adc5e
AdamKershner <adam@kahana.co>
2024-08-22 17:26:23 -0500

    Add blog to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -27,9 +27,9 @@ export default function Footer() {
             <span className="mx-2">|</span>
               <Link href="/explore" className="text-gray-500 hover:text-gray-900">Explore</Link> 
               <span className="mx-2">·</span>
-              <Link href="/faq" className="text-gray-500 hover:text-gray-900">FAQ</Link>
-              <span className="mx-2">·</span>
               <Link href="/pricing" className="text-gray-500 hover:text-gray-900">Pricing</Link>
+              <span className="mx-2">·</span>
+              <Link href="/faq" className="text-gray-500 hover:text-gray-900">FAQ</Link>
               <span className="mx-2">·</span>
               <Link href="/privacy-policy" className="text-gray-500 hover:text-gray-900">Privacy</Link>
               <span className="mx-2">·</span>
@@ -37,17 +37,20 @@ export default function Footer() {
               <span className="mx-2">·</span>
               <Link href="/about" className="text-gray-500 hover:text-gray-900">About</Link> 
               <span className="mx-2">·</span>
+              <Link href="https://blog.kahana.co" className="text-gray-500 hover:text-gray-900">Blog</Link> 
+              <span className="mx-2">·</span>
               <Link href="/sitemap" className="text-gray-500 hover:text-gray-900">Sitemap</Link> 
               <span className="mx-2">·</span>
               <Link href="/sales" className="text-gray-500 hover:text-gray-900">Contact</Link> 
             </span>
             <span className="space-y-2 sm:hidden flex flex-col items-start mt-6">
               <Link href="/explore" className="text-gray-500 hover:text-gray-900">Explore</Link>
-              <Link href="/faq" className="text-gray-500 hover:text-gray-900">FAQ</Link>
               <Link href="/pricing" className="text-gray-500 hover:text-gray-900">Pricing</Link>
+              <Link href="/faq" className="text-gray-500 hover:text-gray-900">FAQ</Link>
               <Link href="/privacy-policy" className="text-gray-500 hover:text-gray-900">Privacy</Link>
               <Link href="/terms-and-conditions" className="text-gray-500 hover:text-gray-900">Terms</Link>
               <Link href="/about" className="text-gray-500 hover:text-gray-900">About</Link>
+              <Link href="https://blog.kahana.co" className="text-gray-500 hover:text-gray-900">Blog</Link>
               <Link href="/sitemap" className="text-gray-500 hover:text-gray-900">Sitemap</Link>
               <Link href="/sales" className="text-gray-500 hover:text-gray-900">Contact</Link>
             </span>
