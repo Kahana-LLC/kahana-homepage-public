d9ce9fbbe7a12ac13168c65c5847bb14fdbedcc1
AdamKershner <adam@kahana.co>
2024-08-25 13:11:28 -0500

    optimized navbar spacing

--- components/NavbarDup.jsx
+++ components/NavbarDup.jsx
@@ -12,10 +12,10 @@ function NavBar() {
 
   return (
     <div>
-      <nav className="bg-white bg-opacity-90">
+      <nav className="fixed top-0 left-0 w-full h-12 bg-white bg-opacity-90 shadow-md z-50">
         <style jsx>{`
           .font-style {
-            font-family: 'YourFontFamily', sans-serif; /* Replace with the actual font family */
+            font-family: 'YourFontFamily', sans-serif;
           }
 
           .mobile-link {
@@ -24,117 +24,138 @@ function NavBar() {
             padding: 0.75rem;
             border-radius: 0.5rem;
             width: 100%;
-            text-align: right; /* Right-align the text */
-            background-color: #f7f7f7; /* Alternate background color */
+            text-align: right;
+            background-color: #f7f7f7;
           }
 
           .mobile-link:nth-child(even) {
-            background-color: #e1e1e1; /* Another shade for alternate rows */
+            background-color: #e1e1e1;
           }
 
           .mobile-link:hover {
             background-color: #D0EDE6;
           }
 
+          .nav-content {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
+            padding: 0 10px;
+          }
+
+          .logo {
+            height: 24px;
+            width: auto;
+          }
+
           .nav-button {
-            padding: 0.3rem 1rem;
+            padding: 0.2rem 1rem;
             border-radius: 0.5rem;
             transition: background-color 0.3s ease, color 0.3s ease;
-            margin-left: 0.5rem; /* Standard spacing between buttons */
+            margin-left: 0.5rem;
           }
 
           .nav-button.explore {
-            background-color: #3B675E; /* Green button color */
+          padding: 0.2rem 1rem;
+            border-radius: 0.5rem;
+            background-color: #3B675E;
             color: white;
           }
 
           .nav-button.explore:hover {
-            background-color: #024324; /* Darker green on hover */
+            background-color: #024324;
           }
 
           .nav-button.app-dashboard {
-            background-color: white; /* White button color */
-            color: #3B675E; /* Green text color */
-            border: 1px solid #3B675E; /* Border to match the text color */
+            padding: 0.2rem 1rem;
+            border-radius: 0.5rem;
+            background-color: white;
+            margin-left: 0.5rem;
+            color: #3B675E;
+            border: 0.5px solid #3B675E;
           }
 
           .nav-button.app-dashboard:hover {
-            background-color: #f0f0f0; /* Light gray on hover */
+            background-color: #f0f0f0;
           }
 
           .hamburger-button {
-            margin-left: 0.5rem; /* Standard spacing between the buttons and the hamburger menu */
+            width: 24px;
+            height: 24px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-left: 1rem;
           }
         `}</style>
-        <div className="container mx-auto px-4">
-          <div className="flex justify-between items-center py-2">
-            {/* Logo */}
-            <div className="flex items-center">
-              <Link href="/" aria-label="Home">
-                <span className="sr-only">Company</span>
-                <Image
-                  className="h-6"
-                  src={whiteKahanaLogo}
-                  alt="navbar-logo"
-                />
-              </Link>
-            </div>
+        <div className="nav-content">
+          {/* Logo */}
+          <Link href="/" aria-label="Home">
+            <span className="sr-only">Company</span>
+            <Image
+              src={whiteKahanaLogo}
+              alt="navbar-logo"
+              className="logo"
+              height={24}
+              width={100}  // Adjust this based on your logo's aspect ratio
+            />
+          </Link>
 
-            {/* Buttons and Hamburger Menu */}
-            <div className="flex items-center">
-              {/* Buttons */}
-              <Link href="/explore">
-                <button className="nav-button explore">Explore</button>
-              </Link>
-              <Link href="https://app.kahana.co">
-                <button className="nav-button app-dashboard">Build</button>
-              </Link>
+          {/* Buttons and Hamburger Menu */}
+          <div className="flex items-center">
+            {/* Buttons */}
+            <Link href="/explore">
+              <button className="nav-button explore">Explore</button>
+            </Link>
+            <Link href="https://app.kahana.co">
+              <button className="nav-button app-dashboard">Build a hub</button>
+            </Link>
 
-              {/* Hamburger Menu */}
-              <button
-                onClick={toggleMobileMenu}
-                className="hamburger-button"
-                aria-label="Mobile Menu"
-              >
-                {isMobileMenuOpen ? (
-                  <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="black"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    ></path>
-                  </svg>
-                ) : (
-                  <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="black"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                  </svg>
-                )}
-              </button>
-            </div>
+            {/* Hamburger Menu */}
+            <button
+              onClick={toggleMobileMenu}
+              className="hamburger-button"
+              aria-label="Mobile Menu"
+            >
+              {isMobileMenuOpen ? (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="black"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="black"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              )}
+            </button>
           </div>
         </div>
 
         {/* Mobile Menu Content */}
         {isMobileMenuOpen && (
-          <div className="h-screen bg-white py-4 z-100 font-style">
+          <div className="fixed top-12 left-0 w-full h-screen bg-white py-4 z-40 font-style">
             <div className="flex flex-col items-end pr-4 space-y-6">
               <Link href="/explore" className="mobile-link">Explore</Link>
               <Link href="https://app.kahana.co" className="mobile-link">App Home</Link>
--- components/navbarexplore.jsx
+++ components/navbarexplore.jsx
@@ -12,10 +12,10 @@ function NavBarExplore() {
 
   return (
     <div>
-      <nav className="bg-white bg-opacity-90">
+      <nav className="fixed top-0 left-0 w-full h-12 bg-white bg-opacity-90 shadow-md z-50">
         <style jsx>{`
           .font-style {
-            font-family: 'YourFontFamily', sans-serif; /* Replace with the actual font family */
+            font-family: 'YourFontFamily', sans-serif;
           }
 
           .mobile-link {
@@ -24,110 +24,97 @@ function NavBarExplore() {
             padding: 0.75rem;
             border-radius: 0.5rem;
             width: 100%;
-            text-align: right; /* Right-align the text */
-            background-color: #f7f7f7; /* Alternate background color */
+            text-align: right;
+            background-color: #f7f7f7;
           }
 
           .mobile-link:nth-child(even) {
-            background-color: #e1e1e1; /* Another shade for alternate rows */
+            background-color: #e1e1e1;
           }
 
           .mobile-link:hover {
             background-color: #D0EDE6;
           }
 
-          .nav-button {
-            padding: 0.3rem 1rem;
-            border-radius: 0.5rem;
-            transition: background-color 0.3s ease, color 0.3s ease;
-            margin-left: 0.5rem; /* Standard spacing between buttons */
+          .nav-content {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
+            padding: 0 10px;
           }
 
-          .nav-button.explore {
-            background-color: #3B675E; /* Green button color */
-            color: white;
-          }
-
-          .nav-button.explore:hover {
-            background-color: #024324; /* Darker green on hover */
-          }
-
-          .nav-button.app-dashboard {
-            background-color: white; /* White button color */
-            color: #3B675E; /* Green text color */
-            border: 1px solid #3B675E; /* Border to match the text color */
-          }
-
-          .nav-button.app-dashboard:hover {
-            background-color: #f0f0f0; /* Light gray on hover */
+          .logo {
+            width: 24px;
+            height: 24px;
           }
 
           .hamburger-button {
-            margin-left: 0.5rem; /* Standard spacing between the buttons and the hamburger menu */
+            width: 24px;
+            height: 24px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
           }
         `}</style>
-        <div className="container mx-auto px-4">
-          <div className="flex items-center py-2">
-            {/* Logo */}
-            <div className="flex items-center flex-grow-0">
-              <Link href="https://app.kahana.co" aria-label="Home">
-                <span className="sr-only">Home</span>
-                <Image
-                  className="h-6"
-                  src={whiteKahanaLogo}
-                  alt="Kahana Logo"
-                />
-              </Link>
-            </div>
+        <div className="nav-content">
+          {/* Logo */}
+          <Link href="/" aria-label="Kahana">
+            <span className="sr-only">Home</span>
+            <Image
+              src={whiteKahanaLogo}
+              alt="Kahana Logo"
+              width={24}
+              height={24}
+              className="logo"
+            />
+          </Link>
 
-            {/* Buttons and Hamburger Menu */}
-            <div className="flex items-center ml-auto">
-              <button
-                onClick={toggleMobileMenu}
-                className="hamburger-button ml-4"
-                aria-label="Mobile Menu"
+          {/* Hamburger Menu */}
+          <button
+            onClick={toggleMobileMenu}
+            className="hamburger-button"
+            aria-label="Mobile Menu"
+          >
+            {isMobileMenuOpen ? (
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="black"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                {isMobileMenuOpen ? (
-                  <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="black"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    ></path>
-                  </svg>
-                ) : (
-                  <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="black"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="black"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            )}
+          </button>
         </div>
 
         {/* Mobile Menu Content */}
         {isMobileMenuOpen && (
-          <div className="h-screen bg-white py-4 z-100 font-style">
+          <div className="fixed top-12 left-0 w-full h-screen bg-white py-4 z-40 font-style">
             <div className="flex flex-col items-end pr-4 space-y-6">
-              <Link href="https://app.kahana.co" className="mobile-link">Home</Link>
+              <Link href="https://app.kahana.co" className="mobile-link">Dashboard</Link>
               <Link href="https://app.kahana.co/billing" className="mobile-link">Billing</Link>
               <Link href="https://blog.kahana.co" className="mobile-link">Blog</Link>
               <Link href="/pricing" className="mobile-link">Pricing</Link>
