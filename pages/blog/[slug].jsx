e2a26cb549682b57e7de1b3d61a0e30d8bfeeb6b
Adam Kershner <79613708+AdamKershner@users.noreply.github.com>
2025-09-17 13:52:28 -0500

    Revert "August 2025 rajshree"

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -339,12 +339,7 @@ export default function BlogPost({ post }) {
 }
 
 export async function getStaticPaths() {
-          {/* Render Browser Comparison Table at the bottom if enabled in JSON */}
-          {post.showBrowserComparisonTable && (
-            <div className="mt-12">
-              <BlogBrowserComparison />
-            </div>
-          )}
+  try {
     // Generate paths from blog-index.js
     const paths = blogIndex.map((post) => ({
       params: { slug: post.slug },
